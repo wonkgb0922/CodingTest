f5859682c92e679b313abffbe11f7de341dbbd47
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2022-10-23 21:37:07 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+
+#pragma warning (disable:4996)
+
+using namespace std;
+
+int main(void) {
+    int a, b;
+    int i;
+    int min=1, max=1;
+    cin >> a >> b;
+    if (a > b) swap(a, b);
+    for (i = 2;i <= a; i++)
+    {
+        if (a % i == 0 && b % i == 0)
+        {
+            min *= i;
+            a /= i;
+            b /= i;
+            i = 1;
+            continue;
+        }
+    }
+    max = min * a * b;
+
+    printf("%d\n%d", min, max);
+    
+
+    return 0;
+}
