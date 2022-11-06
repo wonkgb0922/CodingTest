702fe5c768d60e33ac9060e3f569f0656a840185
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2022-11-07 01:07:28 +0900

    [Silver III] Title: 터렛, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.cc
+++ 백준/Silver/1002. 터렛/터렛.cc
@@ -1,27 +1,29 @@
 #include <stdio.h>
-#include <string.h>
 #include <cmath>
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <set>
+#include <queue>
 #include <map>
-#define PI 3.141592653589793
+#include <set>
 
 #pragma warning (disable:4996)
 
 using namespace std;
 
 int main(void) {
-    int n;
-    long long x1, y1, r1, x2, y2, r2;
+    int t;
+    long long x1, y1, r1;
+    long long x2, y2, r2;
+
     int res;
 
-    cin >> n;
-
-    while (n--)
+    cin >> t;
+    while (t--)
     {
-        cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+        cin >> x1 >> y1 >> r1;
+        cin >> x2 >> y2 >> r2;
+
         if (x1 == x2 && y1 == y2)
         {
             if (r1 == r2) res = -1;
@@ -29,23 +31,18 @@ int main(void) {
         }
         else
         {
-            long long dx = (x1 - x2) * (x1 - x2);
-            long long dy = (y2 - y1) * (y2 - y1);
-
-            long long dis = dx + dy;
-
-            long long ar = (r1 + r2) * (r1 + r2);
-            long long sr = (r1 - r2) * (r1 - r2);
-
-            //if (dis < 0) dis *= -1;
-            if (dis == ar || dis == sr) res = 1;
-            else if (dis < ar && dis > sr) res = 2;
-            else if (dis < sr || dis > ar) res = 0;
+            long long R1, R2, dis1;
+            R1 = (r1 + r2) * (r1 + r2);
+            R2 = (r1 - r2) * (r1 - r2);
+            
+            dis1 = ((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2));
+            if (R1 == dis1 || R2 == dis1) res = 1;
+            else if (R1 < dis1 || R2 > dis1) res = 0;
+            else res = 2;
         }
-
         printf("%d\n", res);
+       
     }
-
 
     return 0;
 }
