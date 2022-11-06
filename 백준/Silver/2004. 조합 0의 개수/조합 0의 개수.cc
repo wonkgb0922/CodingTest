3d64af663cf187475cc580481bfbcf6da72fc414
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2022-11-07 03:09:24 +0900

    [Silver II] Title: 조합 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.cc
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.cc
@@ -1,70 +1,35 @@
 #include <stdio.h>
+#include <cmath>
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
+#include <map>
+#include <set>
 
 #pragma warning (disable:4996)
 
 using namespace std;
-long long gcd(long long a, long long b)
-{
-    if (a % b) return gcd(b, a % b);
-    return b;
-}
 
-int  ary[1001][1001] = {0};
-
-long long func(int n, int k)
+long long count(long long n, long long k)
 {
-    long long num = 0;
-    for (long long i = k; i <= n; i *= k)
+    long long cnt = 0;
+    long long temp;
+    for (long long i = 1; i <= n; i *= k)
     {
-        num += n / i;
+        cnt += n / i;
     }
-    return num;
+    return cnt;
 }
 
 int main(void) {
-    int t;
-    int n, m;
-    int cnt = 0;
-    long long two = 0, five = 0;
-    int temp, temp2;
+    long long n, k;
+    long long two, five, cnt;
+    cin >> n >> k;
 
-    cin >> n >> m;
+    two = count(n, 2) - count(k, 2) - count(n - k, 2);
+    five = count(n, 5) - count(k, 5) - count(n - k, 5);
 
-    two = func(n, 2) - func(n - m, 2) - func(m, 2);
-    five = func(n, 5) - func(n - m, 5) - func(m, 5);
-
-    // 시간초과 코드
-    /*for(int i = 0; i < m; i++)
-    {
-        temp = n - i;
-        temp2 = i + 1;
-
-        while(temp % 2 == 0)
-        {
-            temp /= 2;
-            two++;
-        }
-        while (temp % 5 == 0)
-        {
-            temp /= 5;
-            five++;
-        }
-        while (temp2 % 2 == 0)
-        {
-            temp2 /= 2;
-            two--;
-        }
-        while (temp2 % 5 == 0)
-        {
-            temp2 /= 5;
-            five--;
-        }
-    }
-    */
-    //cnt = (two > five) ? five : two;
     cout << min(two, five);
     return 0;
 }
