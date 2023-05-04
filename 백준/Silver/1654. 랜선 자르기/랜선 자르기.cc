7392c66b7ae374f6117df44a7e94d2b482bf7c8a
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-05-04 13:19:52 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+int ary[10000];
+
+int main(void)
+{
+	int k, n;
+	long long l = 1, r = 0, mid;
+
+	cin >> k >> n;
+	for (int i = 0; i < k; i++)
+	{
+		scanf("%d", ary + i);
+		if (r < ary[i]) r = ary[i];
+	}
+	int cnt, res = 0;
+	while ( l <= r )
+	{
+		mid = (l + r) / 2 ;
+
+		cnt = 0;
+		for (int i = 0; i < k; i++)
+		{
+			cnt += ary[i] / mid;
+		}
+		if (cnt < n)
+		{
+			r = mid - 1;
+		}
+		else 
+		{
+			l = mid + 1;
+			if (res < mid) res = mid;
+		}
+
+		//printf("%d, %d, %d\n", l, mid, r);
+
+	}
+	cout << res;
+	return 0;
+}
