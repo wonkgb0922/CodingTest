eedd6288484694afd988b884a72a873b01430f31
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-05-08 12:37:23 +0900

    [Gold IV] Title: 공유기 설치, Time: 44 ms, Memory: 3612 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.cc
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int ary[10000];
+
+int main(void)
+{
+	int n, c, input;
+	vector<int> v;
+
+	cin >> n >> c;
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &input);
+		v.push_back(input);
+	}
+	sort(v.begin(), v.end());
+	int l = 1, r = v[n - 1] - v[0];
+
+	int mid = 0, sum, cnt;
+	int point;
+	bool d;
+	int res = -1;
+	while ( l <= r )
+	{
+		mid = (l + r) / 2;
+
+		//printf("%d %d %d\n", l, mid, r);
+		sum = 0;
+		cnt = 1;
+		d = false;
+		point = v[0];
+		for (int i = 1; i < n; i++)
+		{
+			if (v[i] - point >= mid)
+			{
+				
+				cnt++;
+				point = v[i];
+				
+			}
+		}
+		if (cnt >= c) l = mid + 1;
+		else r = mid - 1;
+		//printf("cnt : %d\n", cnt);
+	}
+	//printf("%d %d %d\n", l, mid, r);
+	cout << r;
+	return 0;
+}
