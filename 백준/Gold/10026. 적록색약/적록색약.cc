c48c6fb8bd8a1c47fe1bbbc82757fd1e10679ced
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-05-12 14:51:44 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2236 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -16,26 +16,18 @@ queue<pair<int, int>> q;
 
 void dfs(const int i, const int j)
 {
-	//cout << "dfs : : " << i << " " << j << ", n : " << n << endl;
-	//bool d = false;
 	pair<int, int> res;
 	visited[i][j] = true;
 	if (i + 1 < n)	// 아래 방향
 	{
-		//cout << "if 내부 : " << i + 1 << " < " << n << endl;
 		if ((ary[i][j] == ary[i + 1][j]) && !visited[i + 1][j])
 		{
-			//cout << "if문 내부2 : " << ary[i][j] << " == " << ary[i + 1][j] << " bool : " << (ary[i][j] == ary[i + 1][j]) << endl;
-			//visited[i + 1][j] = true;z`
-			//d = true;
-			//res = dfs(i + 1, j);
 			dfs(i + 1, j);
 		}
 		else
 		{
 			if (!visited[i + 1][j])
 			{
-				//cout << "아래방향" << i << " " << j << endl;
 				q.push(make_pair(i + 1, j));
 			}
 		}
@@ -44,9 +36,6 @@ void dfs(const int i, const int j)
 	{
 		if (ary[i][j] == ary[i - 1][j] && !visited[i - 1][j])
 		{
-			//visited[i - 1][j] = true;
-			//d = true;
-			//res = dfs(i - 1, j);
 			dfs(i - 1, j);
 		}
 		else
@@ -61,9 +50,6 @@ void dfs(const int i, const int j)
 	{
 		if (ary[i][j] == ary[i][j + 1] && !visited[i][j + 1])
 		{
-			//visited[i][j + 1] = true;
-			//d = true;
-			//res = dfs(i, j + 1);
 			dfs(i, j + 1);
 		}
 		else
@@ -78,9 +64,6 @@ void dfs(const int i, const int j)
 	{
 		if (ary[i][j] == ary[i][j - 1] && !visited[i][j - 1])
 		{
-			//visited[i][j - 1] = true;
-			//d = true;
-			//res = dfs(i, j - 1);
 			dfs(i, j - 1);
 		}
 		else
@@ -91,32 +74,20 @@ void dfs(const int i, const int j)
 			}
 		}
 	}
-	//if (!d) res = make_pair(i, j);
-	//return res;
 }
 void dfs2(const int i, const int j)	// 색약 dfs
 {
-	//cout << "dfs : : " << i << " " << j << ", n : " << n << endl;
-	bool d = false;
-	//pair<int, int> res;
 	visited[i][j] = true;
 	if (i + 1 < n)	// 아래 방향
 	{
-		//cout << ary[i][j] << " " << ary[i+1][j] << " res : " << (ary[i][j] == 'R' && ary[i + 1][j] == 'G') << " visited : " << visited[i+1][j] <<  endl;
-		//cout << "if 내부 : " << i + 1 << " < " << n << endl;
 		if (((ary[i][j] == ary[i + 1][j]) ||(ary[i][j] == 'G' && ary[i + 1][j] == 'R') ||(ary[i][j] == 'R' && ary[i + 1][j] == 'G')) && (!visited[i + 1][j]))
 		{
-			//cout << "if문 내부2 : " << ary[i][j] << " == " << ary[i + 1][j] << " bool : " << (ary[i][j] == ary[i + 1][j]) << endl;
-			//visited[i + 1][j] = true;
-			d = true;
-			//res = dfs(i + 1, j);
 			dfs2(i + 1, j);
 		}
 		else
 		{
 			if (!visited[i + 1][j])
 			{
-				//cout << "아래방향" << i << " " << j << endl;
 				q.push(make_pair(i + 1, j));
 			}
 		}
@@ -127,9 +98,6 @@ void dfs2(const int i, const int j)	// 색약 dfs
 			(ary[i][j] == 'G' && ary[i - 1][j] == 'R') ||
 			(ary[i][j] == 'R' && ary[i - 1][j] == 'G')) && !visited[i - 1][j])
 		{
-			//visited[i - 1][j] = true;
-			d = true;
-			//res = dfs(i - 1, j);
 			dfs2(i - 1, j);
 		}
 		else
@@ -146,9 +114,6 @@ void dfs2(const int i, const int j)	// 색약 dfs
 			(ary[i][j] == 'G' && ary[i][j + 1] == 'R') ||
 			(ary[i][j] == 'R' && ary[i][j + 1] == 'G')) && !visited[i][j + 1])
 		{
-			//visited[i][j + 1] = true;
-			d = true;
-			//res = dfs(i, j + 1);
 			dfs2(i, j + 1);
 		}
 		else
@@ -165,9 +130,6 @@ void dfs2(const int i, const int j)	// 색약 dfs
 			(ary[i][j] == 'G' && ary[i][j - 1] == 'R') ||
 			(ary[i][j] == 'R' && ary[i][j - 1] == 'G')) && !visited[i][j - 1])
 		{
-			//visited[i][j - 1] = true;
-			d = true;
-			//res = dfs(i, j - 1);
 			dfs2(i, j - 1);
 		}
 		else
@@ -178,30 +140,26 @@ void dfs2(const int i, const int j)	// 색약 dfs
 			}
 		}
 	}
-	//if (!d) res = make_pair(i, j);
-	//return res;
 }
 
 void bfs(int i, int j)
 {
+	pair<int, int> stt;
 	q.push(make_pair(i, j));
 	while (!q.empty())
 	{
-		pair<int, int> stt = q.front();
+		stt = q.front();
 		q.pop();
-		//cout << "q의 front() : " << stt.first << " " << stt.second << endl;
 		if (visited[stt.first][stt.second]) continue;
-		//pair<int,int> end = dfs(stt.first, stt.second);
 		dfs(stt.first, stt.second);
 		res1++;
-		//cout << end.first << " " << end.second << endl;
 	}
 	memset(visited, false, sizeof(visited));
 	q.push(make_pair(i, j));
 
 	while (!q.empty())
 	{
-		pair<int, int> stt = q.front();
+		stt = q.front();
 		q.pop();
 		if (visited[stt.first][stt.second]) continue;
 		dfs2(stt.first, stt.second);
@@ -216,7 +174,6 @@ int main(void)
 	{
 		cin >> ary[i];
 	}
-	//visited[0][0] = true;
 	bfs(0, 0);
 	printf("%d %d", res1, res2);
 	return 0;
