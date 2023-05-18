0f6646c5635b279df8161742b1d4355ecaed4340
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-05-18 14:06:54 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2192 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -8,40 +8,18 @@
 using namespace std;
 
 int dp[10001];
-int n;
-vector<int> v;
 
-int solve(int i)
-{
-	if (dp[i] != -1) return dp[i];
-	if (i <= 2)
-	{
-		if (i == 0) dp[i] = v[0];
-		if (i == 1) dp[i] = v[0] + v[1];
-		if (i == 2)
-		{
-			dp[i] = max(dp[1], v[1] + v[2]);
-			dp[i] = max(dp[i], v[0] + v[2]);
-		}
-		return dp[i];
-	}
-	dp[i] = max(v[i] + solve(i - 2), v[i] + v[i - 1] + solve(i - 3));
-	dp[i] = max(dp[i], solve(i - 1));
-	return dp[i];
-}
 
 int main(void)
 {
-
-	int input;
+	int n, input;
+	vector<int> v;
 	scanf("%d", &n);
-	memset(dp, -1, sizeof(int) * 10001);
 	for (int i = 0; i < n; i++)
 	{
 		scanf("%d", &input);
 		v.push_back(input);
 	}
-	//printf("%d", solve(n - 1));
 	dp[0] = v[0];
 	if (n > 1)
 	{
