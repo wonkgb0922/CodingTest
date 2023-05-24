923d3dc547d51be48847aaf9360f04010fd05924
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-05-24 11:49:18 +0900

    [Silver III] Title: N과 M (5), Time: 40 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <memory.h>
+
+using namespace std;
+
+int n, m;
+vector<int> v;
+int res[8] = { 0 };
+bool visited[8] = { false };
+
+
+void dfs(int cnt)
+{
+	if (cnt >= m)
+	{
+		for (int i = 0; i < m; i++) {
+			printf("%d ", res[i]);
+		}
+		printf("\n");
+		return;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		if (!visited[i]) {
+			res[cnt] = v[i];
+			visited[i] = true;
+			dfs(cnt + 1);
+			visited[i] = false;
+			
+		}
+	}
+}
+
+int main(void)
+{
+	int input;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &input);
+		v.push_back(input);
+	}
+	sort(v.begin(), v.end());
+	dfs(0);
+}
