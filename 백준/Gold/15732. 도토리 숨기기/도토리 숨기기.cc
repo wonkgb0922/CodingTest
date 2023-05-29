c1250bf403c437de03e428a3a274bd746f0a2920
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-05-29 17:03:01 +0900

    [Gold II] Title: 도토리 숨기기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/15732. 도토리 숨기기/도토리 숨기기.cc
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main(void)
+{
+	int l = 1, r, mid, res, temp;
+	int N, K;
+	long long int D, cnt = 0;
+	int ary[10000][3];
+	
+	cin >> N >> K >> D;
+	for (int i = 0; i < K; i++)
+		scanf("%d%d%d", &ary[i][0], &ary[i][1], &ary[i][2]);
+	r = N;
+	while (r >= l)
+	{
+		mid = (l + r) / 2;
+		cnt = 0;
+		for (int i = 0; i < K; i++)
+		{
+			temp = min(ary[i][1], mid);
+			if (ary[i][0] == temp)
+				cnt++;
+			else if (ary[i][0] > temp);
+			else {
+				cnt += (temp - ary[i][0]) / ary[i][2] + 1;
+			}
+		}
+		//printf("%d %d %d, cnt : %d\n", l, mid, r, cnt);
+		if (cnt >= D)
+		{
+			r = mid - 1;
+			res = mid;
+		}
+		else l = mid + 1;
+	}
+	cout << res;
+	return 0;
+}
