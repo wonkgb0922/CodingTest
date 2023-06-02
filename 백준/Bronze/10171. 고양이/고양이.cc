e00c5d21e7d6d8acd1e970335452376fa9817846
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-06-02 17:22:33 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	printf("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+
+	return 0;
+}
