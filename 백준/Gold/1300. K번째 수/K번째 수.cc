6f41bee7e738e915424a3d24b4af8de870186141
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-06-02 23:53:47 +0900

    [Gold II] Title: K번째 수, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.cc
+++ 백준/Gold/1300. K번째 수/K번째 수.cc
@@ -11,7 +11,7 @@ int main()
 	int n, k;
 	int cnt;
 	cin >> n >> k;
-	int l = 1, r = k, mid, res = 0, i;
+	int l = 1, r = k, mid;
 	while (l <= r)
 	{
 		mid = (l + r) / 2;
@@ -19,14 +19,9 @@ int main()
 		for (int i = 1; i <= n; i++)
 			cnt += min(mid / i, n);
 
-		//printf("%lld %lld %lld, %d\n", l, mid, r, cnt);
-		if (cnt >= k)
-		{
-			res = mid;
-			r = mid - 1;
-		}
+		if (cnt >= k) r = mid - 1;
 		else l = mid + 1;
 	}
-	cout << res;
+	cout << l;
 	return 0;
 }
