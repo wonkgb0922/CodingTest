9720305239c95e191df265ab0d24500a883b0d6e
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-06-02 17:26:41 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	printf("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+
+	return 0;
+}
