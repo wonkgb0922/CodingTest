2aff1ce79981e9c2897d51306af4099ad41c39d3
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-06-03 15:08:40 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <list>
+using namespace std;
+
+int main()
+{
+	int n, k, i;
+	cin >> n >> k;
+	vector<int> v, res;
+	for (i = 0; i < n; i++)
+	{
+		v.push_back(i + 1);
+	}
+	i = 0;
+	while (!v.empty())
+	{
+		i += (k - 1);
+		i %= v.size();
+		res.push_back(v[i]);
+		v.erase(v.begin() + i);
+	}
+	printf("<");
+	for (int i = 0; i < res.size(); i++)
+	{
+		printf("%d", res[i]);
+		if (i != res.size()-1) printf(", ");
+	}
+	printf(">");
+
+	return 0;
+}
