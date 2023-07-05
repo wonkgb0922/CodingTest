7cf8f1be01b648396e416977c5efbec7b968b203
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-07-05 22:01:46 +0900

    [Silver IV] Title: 큐, Time: 428 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	string in1;
+	int in2;
+	queue<int> q;
+
+	cin >> n;
+
+	while (n--)
+	{
+		cin >> in1;
+		if (in1 == "push")
+		{
+			cin >> in2;
+			q.push(in2);
+		}
+		else if (in1 == "pop")
+		{
+			if (q.empty()) cout << -1 << '\n';
+			else
+			{
+				cout << q.front() << '\n';
+				q.pop();
+			}
+		}
+		else if (in1 == "size")
+			cout << q.size() << '\n';
+		else if (in1 == "empty")
+			cout << q.empty() << '\n';
+		else if (in1 == "front")
+		{
+			if (q.empty()) cout << -1 << '\n';
+			else cout << q.front() << '\n';
+		}
+		else if (in1 == "back")
+		{
+			if (q.empty()) cout << -1 << '\n';
+			else cout << q.back() << '\n';
+		}
+	}
+	return 0;
+}
