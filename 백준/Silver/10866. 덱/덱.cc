24687df7b17ad6e2afc8f1a41c49a47f782a9b67
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-07-07 14:55:06 +0900

    [Silver IV] Title: 덱, Time: 328 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.cc
+++ 백준/Silver/10866. 덱/덱.cc
@@ -18,36 +18,36 @@ int main()
 	{
 		cin >> in1;
 		if (in1 == "push_front") {
-			cin >> in2;
+			scanf("%d", &in2);
 			q.push_front(in2);
 		}
 		else if (in1 == "push_back") {
-			cin >> in2;
+			scanf("%d", &in2);
 			q.push_back(in2);
 		}
 		else if (in1 == "pop_front") {
 			if (!q.empty()) {
-				cout << q.front() << '\n';
+				printf("%d\n", q.front());
 				q.pop_front();
 			}
 			else cout << -1 << '\n';
 		}
 		else if (in1 == "pop_back") {
 			if (!q.empty()) {
-				cout << q.back() << '\n';
+				printf("%d\n", q.back());
 				q.pop_back();
 			}
-			else cout << -1 << '\n';
+			else printf("-1\n");
 		}
-		else if (in1 == "size") cout << q.size() << '\n';
-		else if (in1 == "empty") cout << q.empty() << '\n';
+		else if (in1 == "size") printf("%d\n", q.size());
+		else if (in1 == "empty") printf("%d\n", q.empty());
 		else if (in1 == "front") {
-			if (!q.empty()) cout << q.front() << '\n';
+			if (!q.empty()) printf("%d\n", q.front());
 			else cout << -1 << '\n';
 		}
 		else if (in1 == "back") {
-			if (!q.empty()) cout << q.back() << '\n';
-			else cout << -1 << '\n';
+			if (!q.empty()) printf("%d\n", q.back());
+			else printf("-1\n");
 		}
 	}
 	return 0;
