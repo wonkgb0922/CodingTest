e0e85562f659e64f14381e6ee538b597df744af7
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-07-09 13:59:36 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	if (n) {
+		cout << "Leading the Way to the Future";
+	}
+	else cout << "YONSEI";
+	return 0;
+}
