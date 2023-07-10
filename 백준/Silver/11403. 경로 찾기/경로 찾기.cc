f4e5a2f99e77852f999ae85a8903d1c320ff2d1d
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-07-10 17:33:25 +0900

    [Silver I] Title: 경로 찾기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	int ary[100][100] = { 0, };
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			scanf("%d", &ary[i][j]);
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < n; k++) {
+				if (ary[j][i] + ary[i][k] > 1) ary[j][k] = 1;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%d ", ary[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
