25b0fb094a9498cf8ea41217737ac53ee0b78a63
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-07-18 01:13:03 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n, m, in1, in2, in3;
+	int ary[101] = { 0, };
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++)
+		ary[i] = i;
+	while(m--) {
+		cin >> in1 >> in2;
+		swap(ary[in1], ary[in2]);
+	}
+	for (int i = 1; i <= n; i++)
+		printf("%d ", ary[i]);
+	return 0;
+}
