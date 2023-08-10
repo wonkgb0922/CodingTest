0c7cbd09a626fe30744686b15d7ccb8df0c73c0b
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-08-10 14:26:03 +0900

    [Gold II] Title: 퍼즐, Time: 244 ms, Memory: 16812 KB -BaekjoonHub

--- 백준/Gold/1525. 퍼즐/퍼즐.cc
+++ 백준/Gold/1525. 퍼즐/퍼즐.cc
@@ -8,79 +8,50 @@
 
 using namespace std;
 
-const int dir[] = { 3,-3 };
-queue<pair<string, int>> q;
-set<string> s;
+const int dir[4][2] = { {0,1},{0,-1},{1,0},{-1,0 } };
+queue<string> q;
+set<string> visited;
 
 int main()
 {
-	int in, qsize, level = -1, pos;
+	int in, level = -1, qsize, pos, posx, posy, nx, ny;
 	string temp = "", next;
-	char swap;
 	bool sol = false;
 	for (int i = 0; i < 9; i++) {
 		scanf("%d", &in);
-		if (in == 0) pos = i;
-		temp += to_string(in);
+		temp += '0' + in;
 	}
-	q.push({ temp , pos });
-	s.insert(temp);
+	q.push({ temp , 0 });
+	visited.insert(temp);
+
 	while (!q.empty()) {
-		qsize = q.size();
 		level++;
-		//printf("------------------%d %d, set의 크기 : %d\n", level, qsize, s.size());
+		qsize = q.size();
 		while (qsize--) {
-			temp = q.front().first;
-			pos = q.front().second;
-			q.pop();
-			/*for (int i = 0; i < 9; i++) {
-				printf("%c ", temp[i]);
-				if (i % 3 == 2) printf("\n");
-			}
-			printf("pos : %d, qsize : %d\n\n", pos, qsize);*/
+			temp = q.front();
 			if (temp == "123456780") {
-				sol = true;
-				break;
+				printf("%d", level);
+				return 0;
 			}
-			// 오른쪽과 교환
-			if (pos % 3 != 2) {
-				next = temp;
-				swap = next[pos];
-				next[pos] = next[pos + 1];
-				next[pos + 1] = swap;
-				if (s.find(next) == s.end()) {
-					s.insert(next);
-					q.push({ next, pos + 1});
-				}
-			}
-			// 위아래 교환
-			for (int i = 0; i < 2; i++) {
-				if (pos + dir[i] >= 0 && pos + dir[i] <= 8) {
+			q.pop();
+			pos = temp.find('0');
+			posx = pos % 3;
+			posy = pos / 3;
+			for (int i = 0; i < 4; i++) {
+				nx = posx + dir[i][1];
+				ny = posy + dir[i][0];
+				if (nx >= 0 && nx <= 2 && ny >= 0 && ny <= 2) {
 					next = temp;
-					swap = next[pos];
-					next[pos] = next[pos + dir[i]];
-					next[pos + dir[i]] = swap;
-					if (s.find(next) == s.end()) {
-						s.insert(next);
-						q.push({ next, pos + dir[i] });
+					swap(next[pos], next[ny * 3 + nx]);
+					if (visited.find(next) == visited.end()) {
+						visited.insert(next);
+						q.push(next);
 					}
 				}
 			}
-			// 왼쪽과 교환
-			if (pos % 3 != 0) {
-				next = temp;
-				swap = next[pos];
-				next[pos] = next[pos - 1];
-				next[pos - 1] = swap;
-				if (s.find(next) == s.end()) {
-					s.insert(next);
-					q.push({ next, pos - 1 });
-				}
-			}
 		}
-		if (sol) break;
+		
 	}
-	if (sol) printf("%d", level);
-	else printf("-1");
+	printf("-1");
 	return 0;
 }
