12dbbb8a6d2c0dae9077381047696626002fa011
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-08-17 14:11:57 +0900

    [Silver II] Title: 부분수열의 합, Time: 488 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -16,19 +16,14 @@ int tempi = 0;
 
 void ex(int m)
 {
-    if (m == rep)
-    {
+    if (m == rep) {
         for (int i = 0; i < rep; i++)
-        {
-            //printf("%d ", ary[i]);
             sum += ary[i];
-        }
         if (sum == s) cnt++;
         sum = 0;
         return;
     }
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         if (!visited[i] && tempi <= i)
         {
             tempi = i;
@@ -45,13 +40,11 @@ int main(void) {
     int input;
 
     cin >> n >> s;
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         cin >> input;
         v.push_back(input);
     }
-    for (int i = 1; i <= n; i++)
-    {
+    for (int i = 1; i <= n; i++) {
         rep = i;
         ex(0);
     }
