7c878bb45bcf53b4822445cb10612f218c7631dc
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-08-25 14:04:55 +0900

    [Gold IV] Title: N-Queen, Time: 4464 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int ary[15] = { 0, };
+int n, cnt = 0;
+
+bool valid(int idx, int val) {
+	for (int i = 0; i < n; i++) {
+		if (ary[i] != 0) {
+			if (max(idx - i, i - idx) == max(val - ary[i], ary[i] - val))
+				return false;
+		}
+	}
+	return true;
+}
+
+void dfs(int idx, int val) {
+	ary[idx] = val;
+
+	if (val == n) {
+		cnt++;
+	}
+	for (int i = 0; i < n; i++) {
+		if (ary[i] == 0 && valid(i, val + 1))
+			dfs(i, val + 1);
+	}
+	ary[idx] = 0;
+}
+
+int main()
+{
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		dfs(i, 1);
+	}
+	printf("%d", cnt);
+	return 0;
+}
