b95509a820b5711d7b5b54d86082f085ef5c7ca0
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-08-25 15:38:49 +0900

    [Gold IV] Title: 스도쿠, Time: 80 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.cc
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -2,36 +2,26 @@
 #include <stdio.h>
 #include <iostream>
 #include <algorithm>
-#include <vector>
 
 using namespace std;
 
-vector<pair<int, int>> v;
 int ary[9][9] = { 0, };
-// [][0]에는 누적합을, [][1]에는 해당 구역의 0의 개수 저장
 bool horizen[9][10] = {0,};
 bool vertical[9][10] = {0,};
 bool area[9][10] = {0,};
 
-bool valid(int idx, int val) {
-	
-	return true;
-}
 bool dfs(int idx) {
 	if (idx == 81) {
 		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
+			for (int j = 0; j < 9; j++)
 				printf("%d ", ary[i][j]);
-			}
 			printf("\n");
 		}
 		return true;
 	}
 	int x = idx / 9, y = idx % 9;
-	
-	if (ary[x][y] != 0) {
+	if (ary[x][y] != 0)
 		return dfs(idx + 1);
-	}
 	for (int i = 1; i <= 9; i++) {
 		if (!horizen[x][i] && !vertical[y][i] && !area[(x / 3) * 3 + (y / 3)][i]) {
 			horizen[x][i] = vertical[y][i] = area[(x / 3) * 3 + (y / 3)][i] = true;
@@ -52,7 +42,6 @@ int main()
 			horizen[i][ary[i][j]] = true;
 			vertical[j][ary[i][j]] = true;
 			area[(i / 3) * 3 + (j / 3)][ary[i][j]] = true;
-
 		}
 	}
 	dfs(0);
