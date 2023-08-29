1f75ec32d3a6993d465c59521f90dc95cb7b168f
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-08-29 13:43:07 +0900

    [Silver I] Title: 컴백홈, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1189. 컴백홈/컴백홈.cc
+++ 백준/Silver/1189. 컴백홈/컴백홈.cc
@@ -10,32 +10,22 @@ bool visited[5][5] = { false, };
 int dir[4][2] = { {0,1},{-1,0},{1,0},{0,-1} };
 int r, c, k, cnt = 0;
 
-
-
 void dfs(int x, int y, int fcnt) {
-	visited[y][x] = true;
 	if (x == c - 1 && y == 0 && fcnt == k) {
 		cnt++;
-		visited[y][x] = false;
 		return;
 	}
-	if (fcnt >= k) {
-		visited[y][x] = false;
-		return;
-	}
-	
+	if (fcnt >= k) return;
+	visited[y][x] = true;
 	int ny, nx;
 	for (int i = 0; i < 4; i++) {
 		ny = y + dir[i][0];
 		nx = x + dir[i][1];
 		if (ny >= 0 && ny < r && nx >= 0 && nx < c) {
-			if (!visited[ny][nx] && ary[ny][nx] != 'T') {
-				dfs(nx, ny, fcnt + 1);
-			}
+			if (!visited[ny][nx] && ary[ny][nx] != 'T') dfs(nx, ny, fcnt + 1);
 		}
 	}
 	visited[y][x] = false;
-
 }
 
 int main()
