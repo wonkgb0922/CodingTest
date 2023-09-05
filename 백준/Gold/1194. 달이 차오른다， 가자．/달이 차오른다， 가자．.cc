c564675a17e1dc8f398edd92726fe7db64e7299f
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-09-05 19:51:19 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.cc
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+bool visited[50][50][1 << 6] = { false, };
+short dir[4][2] = { {0,1}, {0,-1}, {1,0},{-1,0} };
+
+int main(void)
+{
+	queue<pair<pair<int, int>,int>> q;
+	short n, m, x, y, key, qsize, level = -1;
+	char ary[50][50];
+	bool sol = false;
+	cin >> n >> m;
+	getchar();
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			scanf("%c", &ary[i][j]);
+			if (ary[i][j] == '0') {
+				x = j;
+				y = i;
+			}
+		}
+		getchar();
+	}
+	q.push({ {x, y}, 0 });
+	while (!q.empty()) {
+		qsize = q.size();
+		level++;
+		while (qsize--) {
+			x = q.front().first.first;
+			y = q.front().first.second;
+			key = q.front().second;
+			visited[y][x][key] = true;
+			q.pop();
+			if (ary[y][x] == '1') {
+				sol = true;
+				break;
+			}
+			for (int i = 0; i < 4; i++) {
+				int nextx = x + dir[i][1];
+				int nexty = y + dir[i][0];
+
+				if (nextx >= 0 && nextx < m && nexty >= 0 && nexty < n) {
+					int nextkey = key;
+					if (ary[nexty][nextx] == '#') continue;
+					if (ary[nexty][nextx] >= 'a' && ary[nexty][nextx] <= 'f')
+						nextkey |= 1 << (ary[nexty][nextx] - 'a');
+					else if (ary[nexty][nextx] >= 'A' && ary[nexty][nextx] <= 'F') {
+						if (!(key & (1 << (ary[nexty][nextx] - 'A')))) continue;
+					}
+					if (!visited[nexty][nextx][nextkey]) {
+						visited[nexty][nextx][nextkey] = true;
+						q.push({ {nextx, nexty}, nextkey });
+					}
+					
+				}
+			}
+			
+		}
+		if (sol) break;
+	}
+	if (sol) cout << level;
+	else cout << -1;
+	return 0;
+}
