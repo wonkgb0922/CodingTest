2de70ec346de5531ae08dc22c19b3fd86028686c
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-09-09 01:33:30 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 64 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -13,26 +13,19 @@ long long S[100001] = { 0 };
 
 
 int main(void) {
-	int n;
-	int t;
-	int k, l, sum = 0;
+	int n, t, k, l, sum;
 
 	cin >> n >> t;
 	scanf("%d", &ary[0]);
 	S[0] = ary[0];
-	for (int i = 1; i < n; i++)
-	{
+	for (int i = 1; i < n; i++) {
 		scanf("%d", &ary[i]);
 		S[i] = S[i - 1] + ary[i];
 	}
-	while (t--)
-	{
+	while (t--) {
 		scanf("%d %d", &k, &l); // k부터 l까지 사이의 합
 		if (k == 1) sum = S[l - 1];
-		else
-		{
-			sum = S[l - 1] - S[k - 2];
-		}
+		else sum = S[l - 1] - S[k - 2];
 		printf("%d\n", sum);
 	}
 
