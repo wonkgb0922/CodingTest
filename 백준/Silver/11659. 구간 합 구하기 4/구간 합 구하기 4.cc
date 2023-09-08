ba3f67891d8472ef1a2709abc7146971a98d5287
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-09-09 01:37:34 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 64 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,32 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <cmath>
 #include <iostream>
-#include <vector>
-#include <algorithm>
-
-#pragma warning (disable:4996)
+#include <queue>
+#include <map>
 
 using namespace std;
 
-int ary[100001] = { 0 };
 long long S[100001] = { 0 };
 
-
 int main(void) {
-	int n, t, k, l, sum;
+	int n, t, in, s, e;
 
 	cin >> n >> t;
-	scanf("%d", &ary[0]);
-	S[0] = ary[0];
-	for (int i = 1; i < n; i++) {
-		scanf("%d", &ary[i]);
-		S[i] = S[i - 1] + ary[i];
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &in);
+		S[i + 1] = S[i] + in;
 	}
 	while (t--) {
-		scanf("%d %d", &k, &l); // k부터 l까지 사이의 합
-		if (k == 1) sum = S[l - 1];
-		else sum = S[l - 1] - S[k - 2];
-		printf("%d\n", sum);
+		scanf("%d%d", &s, &e);
+		printf("%d\n", S[e] - S[s - 1]);
 	}
 
 	return 0;
