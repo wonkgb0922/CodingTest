9c0adc4b481f369ff2a3893132373828e84b4843
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-09-09 01:13:42 +0900

    [Gold I] Title: 보석 줍기, Time: 384 ms, Memory: 7040 KB -BaekjoonHub

--- 백준/Gold/2001. 보석 줍기/보석 줍기.cc
+++ 백준/Gold/2001. 보석 줍기/보석 줍기.cc
@@ -14,7 +14,7 @@ int n, m, k;
 int main(void)
 {
 	queue<pair<int, pair<int, int>>> q;
-	int in, pos, state, ccnt, qsize, res = 0;
+	int in, pos, state, ccnt, res = 0, next, val;
 	scanf("%d%d%d", &n, &m, &k);
 	for (int i = 0; i < k; i++) {
 		scanf("%d", &in);
@@ -28,35 +28,32 @@ int main(void)
 	q.push({ 1, {0, 0} });
 	visited[1][0] = true;
 	while (!q.empty()) {
-		qsize = q.size();
-		while (qsize--) {
-			pos = q.front().first;
-			state = q.front().second.first;
-			ccnt = q.front().second.second;
-			q.pop();
-			if (pos == 1 && res <= ccnt) {
-				res = ccnt;
-				if (cr.find(1) != cr.end()) {
-					if (!(state & (1 << cr[1]))) res++;
+		pos = q.front().first;
+		state = q.front().second.first;
+		ccnt = q.front().second.second;
+		q.pop();
+		if (pos == 1 && res <= ccnt) {
+			res = ccnt;
+			if (cr.find(1) != cr.end())
+				if (!(state & (1 << cr[1]))) res++;
+		}
+		for (int i = 0; i < weight[pos].size(); i++) {
+			next = weight[pos][i].first;
+			val = weight[pos][i].second;
+			if (cr.find(pos) != cr.end()) {
+				if (val >= ccnt + 1) {
+					if (!visited[next][state | (1 << cr[pos])]){
+						visited[next][state | (1 << cr[pos])] = true;
+						if (state & (1 << cr[pos]))
+							q.push({ next, {state | (1 << cr[pos]), ccnt} });
+						else q.push({ next, {state | (1 << cr[pos]), ccnt + 1} });
+					}
 				}
 			}
-
-			for (int i = 0; i < weight[pos].size(); i++) {
-				if (cr.find(pos) != cr.end()) {
-					if (weight[pos][i].second >= ccnt + 1) {
-						if (!visited[weight[pos][i].first][state | (1 << cr[pos])]){
-							visited[weight[pos][i].first][state | (1 << cr[pos])] = true;
-							if (state & (1 << cr[pos]))
-								q.push({ weight[pos][i].first, {state | (1 << cr[pos]), ccnt} });
-							else q.push({ weight[pos][i].first, {state | (1 << cr[pos]), ccnt + 1} });
-						}
-					}
-				}
-				if (weight[pos][i].second >= ccnt) {
-					if (!visited[weight[pos][i].first][state]) {
-						visited[weight[pos][i].first][state] = true;
-						q.push({ weight[pos][i].first, {state, ccnt} });
-					}
+			if (val >= ccnt) {
+				if (!visited[next][state]) {
+					visited[next][state] = true;
+					q.push({ next, {state, ccnt} });
 				}
 			}
 		}
