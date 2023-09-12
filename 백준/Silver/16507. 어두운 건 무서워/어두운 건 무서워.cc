0cac6fecd9863e298fd4ab8b3db6dcae48c8c212
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-09-13 00:47:16 +0900

    [Silver I] Title: 어두운 건 무서워, Time: 156 ms, Memory: 5936 KB -BaekjoonHub

---
+++ 백준/Silver/16507. 어두운 건 무서워/어두운 건 무서워.cc
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int S[1001][1001] = {0, };
+
+int main(void) {
+	int r, c, q, in;
+	int r1, c1, r2, c2, res;
+	scanf("%d%d%d", &r, &c, &q);
+	for (int i = 0; i < r; i++) {
+		for (int j = 1; j <= c; j++) {
+			scanf("%d", &in);
+			S[i][j] = S[i][j - 1] + in;
+		}
+	}
+	while (q--) {
+		scanf("%d%d%d%d", &r1, &c1, &r2, &c2);
+		res = 0;
+		for (int i = r1 - 1; i <= r2 - 1; i++)
+			res += (S[i][c2] - S[i][c1 - 1]);
+		printf("%d\n", res/ ((r2 - r1 + 1) * (c2 - c1 + 1)));
+	}
+}
