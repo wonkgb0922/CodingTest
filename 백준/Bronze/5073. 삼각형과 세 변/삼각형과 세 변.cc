4c608036db8458b34862f0c334e978f6ebf3eb74
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-09-19 09:56:14 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.cc
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int ary[3];
+	for (;;) {
+		scanf("%d%d%d", &ary[0], &ary[1], &ary[2]);
+		
+		sort(ary, ary + 3);
+		if (ary[0] == ary[1] && ary[1] == ary[2]) {
+			if (ary[0] == 0) break;
+			printf("Equilateral\n");
+		}
+		else {
+			if (ary[2] >= ary[0] + ary[1]) printf("Invalid\n");
+			else if (ary[0] == ary[1] || ary[0] == ary[2] || ary[1] == ary[2])
+				printf("Isosceles\n");
+			else printf("Scalene\n");
+		}
+		
+	}
+	return 0;
+}
