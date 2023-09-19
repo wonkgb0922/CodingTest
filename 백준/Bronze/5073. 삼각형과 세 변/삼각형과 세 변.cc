aa0e0bdbc893af09060abf7e805aa268e750808a
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-09-19 09:56:27 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.cc
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.cc
@@ -8,7 +8,6 @@ int main() {
 	int ary[3];
 	for (;;) {
 		scanf("%d%d%d", &ary[0], &ary[1], &ary[2]);
-		
 		sort(ary, ary + 3);
 		if (ary[0] == ary[1] && ary[1] == ary[2]) {
 			if (ary[0] == 0) break;
@@ -19,8 +18,7 @@ int main() {
 			else if (ary[0] == ary[1] || ary[0] == ary[2] || ary[1] == ary[2])
 				printf("Isosceles\n");
 			else printf("Scalene\n");
-		}
-		
+		}		
 	}
 	return 0;
 }
