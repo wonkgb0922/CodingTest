00d427f2d4755e0d5250b8317f16dfc5f65bad95
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-10-28 17:14:29 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5932 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -4,16 +4,22 @@
 
 using namespace std;
 
-int dp[1001][1001];
+int dp[1000][1000];
 string a, b;
 
 int main()
 {
 	cin >> a >> b;
-	for (int i = 1; i <= a.length(); i++) {
-		for (int j = 1; j <= b.length(); j++)
-			dp[i][j] = max(max(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1] + (a[i - 1] == b[j - 1]));
-	}
-	cout << dp[a.length()][b.length()];
+	dp[0][0] = a[0] == b[0];
+
+	for (int i = 1; i < b.length(); i++)
+		dp[0][i] = max(dp[0][i - 1], (a[0] == b[i]) ? 1 : 0);
+	for (int i = 1; i < a.length(); i++)
+		dp[i][0] = max(dp[i - 1][0], (a[i] == b[0]) ? 1 : 0);
+	for (int i = 1; i < a.length(); i++)
+		for (int j = 1; j < b.length(); j++)
+			dp[i][j] = max(max(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1] + (a[i] == b[j]));
+	cout << dp[a.length() - 1][b.length() - 1];
+
 	return 0;
 }
