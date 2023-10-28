43f79159d0ee1c6f487be46c1145d625d906775b
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-10-28 16:39:40 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -10,8 +10,6 @@ string a, b;
 int main()
 {
 	cin >> a >> b;
-	if (a.length() > b.length()) swap(a, b);
-	
 	for (int i = 1; i <= a.length(); i++) {
 		for (int j = 1; j <= b.length(); j++)
 			dp[i][j] = max(max(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1] + (a[i - 1] == b[j - 1]));
