687e245368d6ea9e5650083c464af72b998f725c
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-11-04 02:14:25 +0900

    [Gold II] Title: 보석 도둑, Time: 376 ms, Memory: 20120 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+
+using namespace std;
+
+struct Pred {
+	bool operator() (pair<int, int> p1, pair<int, int> p2) {
+		if (p1.second == p2.second) {
+			return p1.first > p2.first;
+		}
+		return p1.second < p2.second;
+	}
+};
+
+int main()
+{
+	int n, k, in, in2, val = 0;
+	long long res = 0;
+	priority_queue<pair<int, int>, vector<pair<int, int>>, Pred> q;
+	multiset<int> s;
+	scanf("%d%d", &n, &k);
+	for (int i = 0; i < n; i++) {
+		scanf("%d%d", &in, &in2);
+		q.push({ in,in2 });
+	}
+	for (int i = 0; i < k; i++) {
+		scanf("%d", &in);
+		s.insert(in);
+	}
+	while (!q.empty()) {
+		auto& temp = q.top();
+		auto iter = s.lower_bound(temp.first);
+		if (iter != s.end()) {
+			res += temp.second;
+			s.erase(iter);
+		}
+		q.pop();
+	}
+	cout << res;
+
+	return 0;
+}
