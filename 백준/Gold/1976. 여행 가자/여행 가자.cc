6aed4e638fa3a30644b627ac02c0593a42d8fbee
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-11-05 18:27:18 +0900

    [Gold IV] Title: 여행 가자, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.cc
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <memory.h>
+
+using namespace std;
+
+int p[200];
+
+int find(int n) {
+	if (p[n] < 0) return n;
+	p[n] = find(p[n]);
+	return p[n];
+}
+
+void merge(int a, int b) {
+	a = find(a);
+	b = find(b);
+	if (a == b) return;
+	p[a] += p[b];
+	p[b] = a;
+}
+
+int main()
+{
+	int n, m, in, res = -1;
+	scanf("%d%d", &n, &m);
+	memset(p, -1, sizeof(int)*n);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			scanf("%d", &in);
+			if (in) {
+				merge(i, j);
+			}
+		}
+	}
+	while (m--) {
+		scanf("%d", &in);
+		if (res == -1) {
+			res = find(in - 1);
+		}
+		else {
+			if (res != find(in - 1)) {
+				printf("NO\n");
+				return 0;
+			}
+		}
+	}
+	printf("YES");
+	return 0;
+}
