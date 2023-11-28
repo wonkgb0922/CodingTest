0ff146106f00d8250d8e6dca3f0322224d38617c
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-11-28 09:33:50 +0900

    [Bronze II] Title: 이진수 덧셈, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1252. 이진수 덧셈/이진수 덧셈.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	string a, b, c;
+	int carry = 0, a1, b1, c1;
+	cin >> a >> b;
+	if (a.length() < b.length()) swap(a, b);
+
+	reverse(a.begin(), a.end());
+	reverse(b.begin(), b.end());
+
+	for (int i = 0; i < a.length(); i++) {
+		a1 = a[i] - '0';
+		if (i < b.length()) {
+			b1 = b[i] - '0';
+			c1 = a1 + b1 + carry;
+			if (c1 > 1) {
+				carry = 1;
+				c1 -= 2;
+			}
+			else carry = 0;
+			c.push_back(c1 + '0');
+		}
+		else {
+			c1 = a1 + carry;
+			if (c1 > 1) {
+				carry = 1;
+				c1 -= 2;
+			}
+			else carry = 0;
+			c.push_back(c1 + '0');
+		}
+	}
+	if (carry) c.push_back('1');
+	reverse(c.begin(), c.end());
+	int i = 0;
+	while (c[i++] == '0');
+	i--;
+	if (i == c.length()) c = '0';
+	else c.erase(0, i);
+	cout << c;
+
+	return 0;
+}
