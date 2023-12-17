be3fa7ef826440c304cf9b463fa9cd1044344df4
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2023-12-17 21:19:28 +0900

    [Gold I] Title: 구간 합 구하기, Time: 332 ms, Memory: 25648 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -11,19 +11,17 @@ long long n, m, k, idx;
 void construct() {
 	for (int i = idx - 1; i >= 0; i--)
 		seg[i] = seg[i * 2 + 1] + seg[i * 2 + 2];
-	
 }
 
-void update(long long loc, long long val) {
+void update(int loc, long long val) {
 	loc += idx;
 	seg[loc] = val;
-	for (int i = (loc - 1) / 2; i > 0; i = (i - 1) / 2) {
+	for (int i = (loc - 1) / 2; i > 0; i = (i - 1) / 2)
 		seg[i] = seg[i * 2 + 1] + seg[i * 2 + 2];
-	}
 	seg[0] = seg[1] + seg[2];
 }
 
-long long sol(long long loc, long long l, long long r, long long nodeL, long long nodeR) {
+long long sol(int loc, int l, int r, int nodeL, int nodeR) {
 	if (r < nodeL || nodeR < l) return 0;
 	if (l <= nodeL && nodeR <= r) return seg[loc];
 	int mid = (nodeL + nodeR) / 2;
@@ -36,19 +34,16 @@ int main()
 	cin >> n >> m >> k;
 	idx = ceil(log2(n));
 	idx = pow(2, idx) - 1;
-	for (int i = idx; i < idx + n; i++) {
+	for (int i = idx; i < idx + n; i++)
 		scanf("%lld", seg + i);
-	}
 	construct();
 	for (int i = 0; i < m + k; i++) {
 		scanf("%lld%lld%lld", &in1, &in2, &in3);
 		in2--;
-		if (in1 == 1) {
+		if (in1 == 1)
 			update(in2, in3);
-		}
-		else {	
+		else
 			printf("%lld\n", sol(0, in2, in3 - 1, 0, idx));
-		}
 	}
 	return 0;
 }
