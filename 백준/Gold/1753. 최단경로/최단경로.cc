6ae9c80d0e167404d129e294e7a781e8458a9a8e
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2024-01-03 00:43:49 +0900

    [Gold IV] Title: 최단경로, Time: 152 ms, Memory: 9072 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#define INF 1000000
+
+using namespace std;
+
+vector<pair<int,int>> weight[20000];
+int res[20000];
+
+int main()
+{
+	int v, e, k, in1, in2, in3;
+	priority_queue<pair<int, int>, vector<pair<int,int>>, greater<>> pq;
+	scanf("%d%d", &v, &e);
+	scanf("%d", &k);
+	k--;
+	for (int i = 0; i < v; i++) {
+		res[i] = INF;
+	}
+	for (int i = 0; i < e; i++) {
+		scanf("%d%d%d", &in1, &in2, &in3);
+		in1--, in2--;
+		weight[in1].push_back({ in2,in3 });
+	}
+	res[k] = 0;
+	pq.push({ 0, k });
+
+	while (!pq.empty()) {
+		auto t = pq.top();
+		pq.pop();
+		if (res[t.second] < t.first) continue;
+		for (auto& it : weight[t.second]) {
+			if (res[it.first] > res[t.second] + it.second) {
+				res[it.first] = res[t.second] + it.second;
+				pq.push({ res[it.first], it.first });
+			}
+		}
+	}
+	for (int i = 0; i < v; i++) {
+		if (res[i] == INF) printf("INF\n");
+		else printf("%d\n", res[i]);
+	}
+	return 0;
+}
