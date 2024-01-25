ce32f6769b18c42e9dd380eaeb34a5a04125d1d7
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2024-01-25 18:02:56 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2192 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -1,6 +1,5 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
 #include <stdio.h>
+#include <iostream>
 #include <algorithm>
 #include <vector>
 
@@ -13,8 +12,7 @@ int main(void)
 	int n, input;
 	vector<int> v;
 	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; i++) {
 		scanf("%d", &input);
 		v.push_back(input);
 	}
@@ -22,16 +20,14 @@ int main(void)
 	if (n > 1)
 	{
 		dp[1] = v[0] + v[1];
-		if (n > 2)
-		{
+		if (n > 2) {
 			dp[2] = max(dp[1], v[1] + v[2]);
 			dp[2] = max(dp[2], v[0] + v[2]);
 		}
 	}
-	for (int i = 3; i < n; i++)
-	{
-		dp[i] = max(v[i] + dp[i-2], v[i] + v[i - 1] + dp[i-3]);
-		dp[i] = max(dp[i], dp[i-1]);
+	for (int i = 3; i < n; i++) {
+		dp[i] = max(v[i] + dp[i - 2], v[i] + v[i - 1] + dp[i - 3]);
+		dp[i] = max(dp[i], dp[i - 1]);
 	}
 	cout << dp[n - 1];
 }
