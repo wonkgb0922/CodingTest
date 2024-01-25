755ab1ce0652dfc70b30d2a4485d34b53db6117e
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2024-01-25 16:43:35 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 144 ms, Memory: 11884 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -1,67 +1,42 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <stdio.h>
-#include <vector>
 #include <algorithm>
 #include <queue>
 #include <map>
 
 using namespace std;
 
-vector<pair<int, int>> v;
-multimap<int, int> m1, m2;
+multimap<int, int> m1;
 bool visited[100001] = { false };
 int res[100001] = { 0 };
-void bfs(int num)
-{
-	queue<pair<int,int>> q;
-	int temp = 0;
-
-	q.push(make_pair(num, 0));
-	while (!q.empty())
-	{
-		res[q.front().first] = q.front().second;
-		temp = q.front().first; // 1
-		q.pop();
-		
-		auto range = m1.equal_range(temp);
-		for (auto& it = range.first; it != range.second; it++)
-		{
-			if (!visited[it->second])
-			{
-				visited[it->second] = true;
-				q.push(make_pair(it->second, temp));
-			}
-		}
-		range = m2.equal_range(temp);
-		for (auto& it = range.first; it != range.second; it++)
-		{
-			if (!visited[it->second])
-			{
-				visited[it->second] = true;
-				q.push(make_pair(it->second, temp));
-			}
-		}
-		
-	}	
-}
 
 int main(void)
 {
-	int n;
-	int fir, sec;
+	int n, in, in2;
 	cin >> n;
-	for (int i = 0; i < n - 1; i++)
-	{
-		scanf("%d%d", &fir, &sec);
-		m1.insert({ fir, sec });
-		m2.insert({ sec, fir });
+	for (int i = 0; i < n - 1; i++) {
+		scanf("%d%d", &in, &in2);
+		m1.insert({ in, in2 });
+		m1.insert({ in2, in });
 	}
-	//sort(v.begin(), v.end());
-	bfs(1);
+	queue<pair<int, int>> q;
+	int temp = 0;
+
+	q.push({1,0});
+	while (!q.empty()) {
+		res[q.front().first] = q.front().second;
+		temp = q.front().first;
+		q.pop();
+
+		auto range = m1.equal_range(temp);
+		for (auto& it = range.first; it != range.second; it++) {
+			if (!visited[it->second]) {
+				visited[it->second] = true;
+				q.push({ it->second, temp });
+			}
+		}
+	}
 	for (int i = 2; i <= n; i++)
-	{
 		printf("%d\n", res[i]);
-	}
 	return 0;
 }
