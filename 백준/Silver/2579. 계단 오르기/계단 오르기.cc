1a3a4b9f8976ee7923bb5fdfeba0d949a99f824c
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2024-01-25 18:00:03 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,35 +1,31 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#pragma warning (disable:4996)
 
 using namespace std;
 
-int max_ = 0, dp[301] = { 0 }, n;
+int dp[301] = { 0 }, n;
 vector<int> v;
 
 int solve(int i)
 {
 	if (dp[i] != 0) return dp[i];
-	if(i>=3) dp[i] = v[i] + max(v[i-1] + solve(i - 3), solve(i - 2));
-	else
-	{
+	if (i >= 3) dp[i] = v[i] + max(v[i - 1] + solve(i - 3), solve(i - 2));
+	else {
 		if (i == 0) dp[i] = v[0];
 		if (i == 1) dp[i] = v[0] + v[1];
-		if (i == 2) dp[i] =  max(v[0] + v[2], v[1] + v[2]);
+		if (i == 2) dp[i] = max(v[0], v[1]) + v[2];
 		return dp[i];
 	}
-	//cout << i << ", dp[i] : " << dp[i] << "jump : " << jump <<  endl;
 	return dp[i];
 }
 
 int main(void) {
 	int input;
 	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; i++) {
 		scanf("%d", &input);
 		v.push_back(input);
 	}
-	cout << solve(n-1);
+	cout << solve(n - 1);
 }
