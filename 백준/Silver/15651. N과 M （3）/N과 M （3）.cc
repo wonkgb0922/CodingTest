2d76b5d7206cda22bbbe64feb851c1ee2f38042c
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2024-01-26 00:35:28 +0900

    [Silver III] Title: N과 M (3), Time: 460 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -1,42 +1,26 @@
 #include <stdio.h>
 #include <iostream>
-#include <vector>
-#include <algorithm>
-
-#pragma warning (disable:4996)
 
 using namespace std;
 
-bool visited[9] = { false };
-int ary[9] = { 0 };
-int n, m;
+int ary[8], n, m;
 
 void dfs(int cnt)
 {
-    if (cnt >= m)
-    {
+    if (cnt >= m) {
         for (int i = 0; i < m; i++)
-        {
             printf("%d ", ary[i]);
-            
-        }
         printf("\n");
+        return;
     }
-    else
-    {
-        for (int i = 1; i <= n; i++)
-        {
-            ary[cnt] = i;
-            dfs(cnt + 1);
-        }
+    for (int i = 1; i <= n; i++) {
+        ary[cnt] = i;
+        dfs(cnt + 1);
     }
 }
 
 int main(void) {
-    int ary[8] = { 0 };
     cin >> n >> m;
-    
     dfs(0);
-
     return 0;
 }
