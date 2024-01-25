b6994b7f7801ba5a382944da46d47e218a02930e
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2024-01-25 16:32:27 +0900

    [Silver II] Title: DFS와 BFS, Time: 8 ms, Memory: 3084 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,100 +1,63 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <iostream>
-#include <vector>
+#include <map>
 #include <algorithm>
 #include <queue>
 
 using namespace std;
 
-vector<pair<int, int>> g;
+multimap<int, int> g;
 bool visited[1001];
 
 void dfs(int n)
 {
-	vector<int> vv;
+	vector<int> v;
 	printf("%d ", n);
-	auto d = find_if(g.begin(), g.end(), [n](const pair<int, int>& p) {return p.first == n;});
-	while (d != g.end() && d->first == n)
-	{
-		vv.push_back(d->second);
-		d++;
+	auto rg = g.equal_range(n);
+	for (auto& it = rg.first; it != rg.second; it++) {
+		v.push_back(it->second);
 	}
-	d = find_if(g.begin(), g.end(), [n](const pair<int, int>& p) {return p.second == n; });
-	while (d != g.end())
-	{
-		vv.push_back(d->first);
-		d++;
-		d = find_if(d, g.end(), [n](const pair<int, int>& p) {return p.second == n; });
-	}
-	sort(vv.begin(), vv.end());
-	for (auto& it : vv)
-	{
-		if (!visited[it])
-		{
+	sort(v.begin(), v.end());
+	for (auto it : v) {
+		if (!visited[it]) {
 			visited[it] = true;
 			dfs(it);
 		}
 	}
 }
 
-void bfs(int n)
-{
-	queue<int> q;
-	vector<int> vv;
-	int fir;
-	q.push(n);
-	while (!q.empty())
-	{
-		vv.clear();
-		fir = q.front();
-		printf("%d ", fir);
-		q.pop();
-		auto d = find_if(g.begin(), g.end(), [fir](const pair<int, int>& p) {return p.first == fir; });
-		while (d != g.end() && d->first == fir)
-		{
-			if (!visited[d->second])
-			{
-				visited[d->second] = true;
-				vv.push_back(d->second);
-			}
-			d++;
-		}
-		d = find_if(g.begin(), g.end(), [fir](const pair<int, int>& p) {return p.second == fir; });
-		while (d != g.end())
-		{
-			if (!visited[d->first])
-			{
-				visited[d->first] = true;
-				vv.push_back(d->first);
-			}
-			d++;
-			d = find_if(d, g.end(), [fir](const pair<int, int>& p) {return p.second == fir; });
-		}
-		sort(vv.begin(), vv.end());
-		for (auto it : vv) q.push(it);
-	}
-}
-
 int main(void)
 {
-	//int cnt[1001] = { 0 };
-	int fir, sec;
-	int n, m, v;
-	
+	int in, in2, n, m, v;
+	queue<int> q;
+	vector<int> vv;
 	cin >> n >> m >> v;
-	fill(visited, visited + n + 1, false);
 	for (int i = 0; i < m; i++)
 	{
-		scanf("%d%d", &fir, &sec);
-		g.push_back(make_pair(fir, sec));
-		//cnt[fir]++;
+		scanf("%d%d", &in, &in2);
+		g.insert({ in,in2 });
+		g.insert({ in2,in });
 	}
-	sort(g.begin(), g.end());
 	visited[v] = true;
 	dfs(v);
 	fill(visited, visited + n + 1, false);
 	visited[v] = true;
 	printf("\n");
-	bfs(v);
+	q.push(v);
+	while(!q.empty()) {
+		vv.clear();
+		auto t = q.front();
+		printf("%d ", t);
+		q.pop();
+		auto rg = g.equal_range(t);
+		for (auto& it = rg.first; it != rg.second; it++) {
+			if (!visited[it->second]) {
+				visited[it->second] = true;
+				vv.push_back(it->second);
+			}
+		}
+		sort(vv.begin(), vv.end());
+		for (auto it : vv)
+			q.push(it);
+	}
 }
