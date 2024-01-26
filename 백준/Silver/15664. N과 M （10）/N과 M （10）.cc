21a7ea9cb97661945815f09abdcf573735946579
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2024-01-26 18:27:02 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -1,32 +1,24 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
 #include <stdio.h>
+#include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int n, m, ncnt = 0;
 vector<int> v;
-int vcnt[10001] = { 0 };
-int res[8] = { 0 };
+int vcnt[10001], res[8], n, m, ncnt = 0;
 
 void dfs(int cnt)
 {
-	if (cnt >= m)
-	{
-		for (int i = 0; i < m; i++) {
+	if (cnt >= m) {
+		for (int i = 0; i < m; i++)
 			printf("%d ", res[i]);
-		}
 		printf("\n");
 		return;
 	}
-	for (int i = 0; i < ncnt; i++)
-	{
-		if (vcnt[v[i]] > 0)
-		{
-			if (res[cnt-1] <= v[i] || cnt == 0)
-			{
+	for (int i = 0; i < ncnt; i++) {
+		if (vcnt[v[i]] > 0) {
+			if (cnt == 0 || res[cnt - 1] <= v[i]) {
 				res[cnt] = v[i];
 				vcnt[v[i]]--;
 				dfs(cnt + 1);
@@ -42,8 +34,7 @@ int main(void)
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &input);
-		if (vcnt[input] == 0)
-		{
+		if (vcnt[input] == 0) {
 			v.push_back(input);
 			ncnt++;
 		}
