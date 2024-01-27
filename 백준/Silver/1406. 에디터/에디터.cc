bf31289f06b75502bf5f83d2ce7e3e30c2e88454
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2024-01-27 13:55:53 +0900

    [Silver II] Title: 에디터, Time: 148 ms, Memory: 20880 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,10 +1,7 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <iostream>
 #include <algorithm>
-#include <vector>
 #include <list>
-#include <memory.h>
 
 using namespace std;
 
@@ -20,36 +17,30 @@ int main()
 	for (int i = 0; i < size; i++)
 		l.push_back(s[i]);
 
-	list<char>::iterator it = l.end();
+	auto it = l.end();
 	cin >> m;
-	while (m--)
-	{
+	while (m--) {
 		cin >> in1;
-		if (in1 == 'P')
-		{
+		if (in1 == 'P') {
 			cin >> in2;
 			it = l.insert(it, in2);
 			it++;
 		}
-		else if (in1 == 'L')
-		{
+		else if (in1 == 'L') {
 			if (it != l.begin()) it--;
 		}
-		else if (in1 == 'D')
-		{
+		else if (in1 == 'D') {
 			if (it != l.end()) it++;
 		}
-		else if (in1 == 'B')
-		{
-			if (it != l.begin())
-			{
+		else if (in1 == 'B') {
+			if (it != l.begin()) {
 				it--;
-				it = l.erase(it);				
+				it = l.erase(it);
 			}
-			
 		}
 	}
 	for (auto& x : l)
 		cout << x;
+
 	return 0;
 }
