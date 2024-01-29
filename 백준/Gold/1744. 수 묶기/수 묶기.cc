b0359c6051ca593e8f019a08357f005e3cadfc7d
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2024-01-30 00:59:27 +0900

    [Gold IV] Title: 수 묶기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1744. 수 묶기/수 묶기.cc
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, ary[50], res = 0;
+    bool isMinus = false, flag = false;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", ary + i);
+        if (ary[i] < 0) isMinus = !isMinus;
+    }
+    sort(ary, ary + n, greater<>());
+    for (int i = 0; i < n; i++) {
+        if (ary[i] > 1) {
+            if (i + 1 < n && ary[i + 1] > 1) {
+                res += ary[i] * ary[i + 1];
+                i++;
+            }
+            else
+                res += ary[i];
+        }
+        else if (ary[i] == 1) res += ary[i];
+        else if (ary[i] == 0) {
+            if (isMinus) flag = true;
+        }
+        else {
+            if (flag) {
+                flag = false;
+                isMinus = false;
+            }
+            else {
+                if (isMinus) {
+                    res += ary[i];
+                    isMinus = false;
+                }
+                else {
+                    if (i + 1 < n && ary[i + 1] < 0) {
+                        res += ary[i] * ary[i + 1];
+                        i++;
+                    }
+                    else res += ary[i];
+                }
+            }
+        }
+       // printf("%d\n", res);
+    }
+    printf("%d", res);
+
+    return 0;
+}
