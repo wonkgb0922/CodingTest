7a8c32b6b62679cae35378e50daf6f03bd96e51a
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2024-01-30 14:57:43 +0900

    [Silver I] Title: 동물원, Time: 4 ms, Memory: 7364 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.cc
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -4,27 +4,22 @@
 
 using namespace std;
 
-int n, dp[100000][3];
+int n, dp[100000][2];
 
 int sol(int idx, int loc)
 {
     if (idx == 0) return 1;
     if (dp[idx][loc]) return dp[idx][loc];
-    if (loc == 0) {
-        dp[idx][loc] = sol(idx - 1, 0) + sol(idx - 1 , 1) + sol(idx - 1, 2);
-    }
-    else if (loc == 1) {
-        dp[idx][loc] = sol(idx - 1, 0) + sol(idx - 1, 2);
-    }
-    else {
+    if (loc == 0)
+        dp[idx][loc] = sol(idx - 1, 0) + sol(idx - 1 , 1) * 2;
+    else
         dp[idx][loc] = sol(idx - 1, 0) + sol(idx - 1, 1);
-    }
     dp[idx][loc] %= 9901;
     return dp[idx][loc];
 }
 
 int main() {
     cin >> n;
-    cout << (sol(n - 1, 0) + sol(n - 1, 1) + sol(n - 1, 2)) % 9901;
+    cout << (sol(n - 1, 0) + sol(n - 1, 1) * 2) % 9901;
     return 0;
 }
