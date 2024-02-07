a282c28d9699730cfafa6cdafd680c60bdd19a3f
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2024-02-07 15:17:04 +0900

    [Gold V] Title: 회문, Time: 104 ms, Memory: 2272 KB -BaekjoonHub

---
+++ 백준/Gold/17609. 회문/회문.cc
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+string s;
+
+bool isPal(int i, int j)
+{
+	while (i < j) {
+		if (s[i] == s[j]) {
+			i++;
+			j--;
+		}
+		else return false;
+	}
+	return true;
+}
+
+int main()
+{
+	int t, i, j, res;
+	cin >> t;
+	while (t--) {
+		cin >> s;
+		res = 0;
+		for (i = 0, j = s.length() - 1; i < j;) {
+			if (s[i] == s[j]) {
+				i++;
+				j--;
+			}
+			else {
+				if (isPal(i + 1, j) || isPal(i, j - 1)) {
+					res = 1;
+					break;
+				}
+				else {
+					res = 2;
+					break;
+				}
+			}
+		}
+		cout << res << "\n";
+	}
+	return 0;
+}
