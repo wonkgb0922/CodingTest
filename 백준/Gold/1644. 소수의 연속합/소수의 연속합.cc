fba5a4059a710ba5202da90092ed55e422a335bc
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2024-02-12 01:39:02 +0900

    [Gold III] Title: 소수의 연속합, Time: 128 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <memory.h>
+#define MAX 4000001
+
+using namespace std;
+
+bool isPrime[MAX];
+
+int main()
+{
+	int n, i, j, cnt = 0, sum;
+	cin >> n;
+	memset(isPrime, 1, sizeof(isPrime));
+	for (i = 2; i < MAX / 2; i++) {
+		for (j = 2; i * j < MAX; j++) {
+			isPrime[i * j] = false;
+		}
+	}
+	sum = 2;
+	for (i = 2, j = 2; i <= j; ) {
+		if (sum < n) {
+			j++;
+			while (!isPrime[j]) j++;
+			sum += j;
+		}
+		else if (sum > n) {
+			sum -= i;
+			i++;
+			while (!isPrime[i]) i++;
+		}
+		else {
+			cnt++;
+			j++;
+			while (!isPrime[j]) j++;
+			sum += j;
+		}
+	}
+	cout << cnt;
+	return 0;
+}
