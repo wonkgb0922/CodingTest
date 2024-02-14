f0d86fa59c8606a6b0f8f852794b8489506f22ad
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2024-02-14 21:49:30 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 252 ms, Memory: 8176 KB -BaekjoonHub

---
+++ 백준/Gold II/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+vector<int> v;
+
+int main()
+{
+	int n, in, j, res = 0;
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &in);
+		auto t = lower_bound(v.begin(), v.end(), in);
+		if (t != v.end()) *t = in;
+		else v.push_back(in);
+	}
+	cout << v.size();
+
+	return 0;
+}
