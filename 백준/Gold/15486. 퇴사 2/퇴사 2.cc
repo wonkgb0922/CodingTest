7b29aba6c88f2f919f6f3cbcfed2bf7c914cf9cc
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2024-03-09 16:58:37 +0900

    [Gold V] Title: 퇴사 2, Time: 388 ms, Memory: 94464 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int dp[1500000], n;
+vector<pair<int, int>> v;
+
+int sol(int idx) {
+	if (idx == n) return 0;
+	if (dp[idx]) return dp[idx];
+	if (idx + v[idx].first <= n) {
+		dp[idx] = max(sol(idx + v[idx].first) + v[idx].second, sol(idx + 1));
+	}
+	else if (idx + 1 <= n) {
+		dp[idx] = sol(idx + 1);
+	}
+	return dp[idx];
+}
+
+int main()
+{
+	int in1, in2;
+
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d%d", &in1, &in2);
+		v.push_back({ in1, in2 });
+	}
+	cout << sol(0);
+}
