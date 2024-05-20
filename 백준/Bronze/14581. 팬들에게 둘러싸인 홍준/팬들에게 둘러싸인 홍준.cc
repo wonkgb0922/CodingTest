97316f0ae2c8f46734007f7d89003a64b48418d7
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2024-05-21 02:04:08 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << s << "::fan:\n";
+    cout << ":fan::fan::fan:";
+    
+    return 0;
+}
