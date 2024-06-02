4599b0e3af1bf299fe632a4a69f1c825a37e3ab8
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2024-06-03 00:30:59 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -1,17 +1,12 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <iostream>
-#include <algorithm>
-#include <string>
 
 using namespace std;
 
 int main()
 {
-	char c;;
-	while (scanf("%c", &c) != EOF) {
+	char c;
+	while (scanf("%c", &c) != EOF)
 		printf("%c", c);
-	}
 
 	return 0;
 }
