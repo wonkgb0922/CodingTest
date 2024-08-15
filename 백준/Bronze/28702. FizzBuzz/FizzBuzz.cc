fe15c63a97ef9091227adc78e44e8cba94593504
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2024-08-15 17:27:06 +0900

    [Bronze I] Title: FizzBuzz, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.cc
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string a, b, c;
+	int ai = 0, bi = 0, ci = 0;
+	int d = 0;
+	cin >> a >> b >> c;
+	if (a[0] > '0' && a[0] <= '9')
+		ai = stoi(a);
+	if (b[0] > '0' && b[0] <= '9')
+		bi = stoi(b);
+	if (c[0] > '0' && c[0] <= '9')
+		ci = stoi(c);
+
+	if (ai) d = ai + 3;
+	else if (bi) d = bi + 2;
+	else if (ci) d = ci + 1;
+	if (!(d % 3) && !(d % 5)) printf("FizzBuzz");
+	else if (!(d % 3)) printf("Fizz");
+	else if (!(d % 5)) printf("Buzz");
+	else printf("%d", d);
+	return 0;
+}
