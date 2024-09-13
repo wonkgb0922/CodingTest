8d7c486ae4de4f19c83bed0900b8fdd2bb470e6a
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2024-09-13 14:50:18 +0900

    [Gold III] Title: 내리막 길, Time: 28 ms, Memory: 4528 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -5,19 +5,20 @@
 
 using namespace std;
 
+int dp[500][500], ary[500][500], dir[4][2] = { {0, 1}, {0, -1}, {1, 0}, {-1, 0} };
 int n, m;
-int ary[500][500], dir[4][2] = { {0,1},{0,-1},{1,0},{-1,0} }, dp[500][500];
 
-int dfs(int i, int j) {
-	if (i == n - 1 && j == m - 1) return 1;
-	if (dp[i][j] != -1) return dp[i][j];
+int sol(int i, int j)
+{
+	if (!i && !j) return 1;
+	if (dp[i][j] > -1) return dp[i][j];
 	dp[i][j] = 0;
 	for (int k = 0; k < 4; k++) {
-		int ni = i + dir[k][0];
-		int nj = j + dir[k][1];
-		if (ni >= 0 && ni < n && nj >= 0 && nj < m) {
-			if (ary[i][j] > ary[ni][nj])
-				dp[i][j] += dfs(ni, nj);
+		int ti = i + dir[k][0];
+		int tj = j + dir[k][1];
+		if (ti >= 0 && ti < n && tj >= 0 && tj < m) {
+			if (ary[ti][tj] > ary[i][j])
+				dp[i][j] += sol(ti, tj);
 		}
 	}
 	return dp[i][j];
@@ -25,11 +26,11 @@ int dfs(int i, int j) {
 
 int main()
 {
-	memset(dp, -1, sizeof(dp));
-	scanf("%d%d", &n, &m);
+	cin >> n >> m;
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++)
 			scanf("%d", &ary[i][j]);
-
-	cout << dfs(0, 0);
+	memset(dp, -1, sizeof(dp));
+	cout << sol(n - 1, m - 1);
+	return 0;
 }
