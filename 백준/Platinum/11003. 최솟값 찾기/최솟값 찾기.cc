c56b5776de32814cbee770dcfa014d473d0af701
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2025-03-26 12:44:15 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2276 ms, Memory: 100340 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <iostream>
 #include <algorithm>
-#include <deque>
 #include <queue>
 
 using namespace std;
 
 int main()
 {
-	deque<int> dq;
 	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> q;
 
 	int n, l, input;
@@ -16,8 +14,6 @@ int main()
 	cin >> n >> l;
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &input);
-
-		dq.push_back(input);
 		q.push({ input , i });
 
 		auto t = q.top();
