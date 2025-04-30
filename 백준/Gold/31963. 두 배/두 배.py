83f4da638de4a756f5aadb6a4c589b8ca21b5d01
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2025-05-01 01:59:32 +0900

    [Gold IV] Title: 두 배, Time: 268 ms, Memory: 59396 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.py
+++ 백준/Gold/31963. 두 배/두 배.py
@@ -1,10 +1,10 @@
-import math
+from math import *
 n = int(input())
 l = list(map(int, input().split()))
 ary = [0 for i in range(n)]
 cnt = 0
 x = 0
 for i in range(1, len(l)):
-  ary[i] = math.ceil(math.log2(l[i - 1] / l[i])) + ary[i - 1]
+  ary[i] = max(ceil(log2(l[i - 1] / l[i])) + ary[i - 1], 0)
   cnt += ary[i]
-print(max(cnt, 0))
+print(cnt)
