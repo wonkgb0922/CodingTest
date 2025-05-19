8c1c5f57009a75a86a2015b3d93610d028a8a84e
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2025-05-19 16:55:04 +0900

    [Gold III] Title: 나머지 합, Time: 196 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.cc
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -1,22 +1,20 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <iostream>
 
 using namespace std;
 
-int S[1000001] = { 0, };
-int mcnt[1000] = { 0, };
+int mcnt[1000];
 
-int main(void) {
-	int n, m;
-	long long int in, cnt = 0;
+int main() {
+	int n, m, t = 0;
+	long long in, cnt = 0;
 
 	scanf("%d%d", &n, &m);
 	mcnt[0] = 1;
-	for (int i = 1; i <= n; i++) {
-		scanf("%d", &in);
-		S[i] = (S[i - 1] + in) % m;
-		cnt += mcnt[S[i]]++;
+	for (int i = 0; i < n; i++) {
+		scanf("%lld", &in);
+		t = (t + in) % m;
+		cnt += mcnt[t]++;
 	}
 	cout << cnt;
 	return 0;
