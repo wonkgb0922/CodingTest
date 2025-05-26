7944ad8e9309c937273988ca640e2ab33b695823
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2025-05-26 19:51:45 +0900

    [Silver III] Title: 수열, Time: 80 ms, Memory: 41104 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -2,7 +2,7 @@ n, k = map(int, input().split())
 ary = list(map(int, input().split()))
 S = [0]; [S.append(S[-1] + x) for x in ary]
 
-res = []
+res = -10000000000
 for i in range(k, n + 1):
-  res.append(S[i]- S[i - k])
-print(max(res))
+  res = max(res, S[i]- S[i - k])
+print(res)
