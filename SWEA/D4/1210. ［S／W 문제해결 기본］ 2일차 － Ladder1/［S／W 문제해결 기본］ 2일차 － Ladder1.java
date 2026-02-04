f6aa14f8c6dae4868be90bab21cd9afedc556daa
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-02-04 11:48:47 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 206 ms, Memory: 35,024 KB -BaekjoonHub

---
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int T = 10;
+        int t, i, j, s = 0;
+        int ary[][] = new int[100][100];
+        
+        while(T-- > 0) {
+        	t = Integer.parseInt(br.readLine());
+        	for(i = 0; i < 100; i++) {
+        		st = new StringTokenizer(br.readLine());
+        		for(j = 0; j < 100; j++) {
+        			ary[i][j] = Integer.parseInt(st.nextToken());
+        			if(ary[i][j] == 2) {
+        				s = j;
+        			}
+        		}
+        	}
+        	i = 99;
+        	j = s;
+        	while(i > 0) {
+        		if(j + 1 < 100 && ary[i][j + 1] == 1) {
+        			ary[i][j] = 0;
+        			j++;
+        		}
+        		else if(j - 1 >= 0 && ary[i][j - 1] == 1) {
+        			ary[i][j] = 0;
+        			j--;
+        		}
+        		else
+        			i--;
+        	}        	
+            sb.append("#").append(t).append(" ").append(j).append("\n");
+        }        
+        System.out.println(sb);
+    }
+}
