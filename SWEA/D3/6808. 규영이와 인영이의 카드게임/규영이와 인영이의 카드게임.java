97db6d85962b063489cea31093f0c9689821629f
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-02-05 00:37:33 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 908 ms, Memory: 28,224 KB -BaekjoonHub

---
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -0,0 +1,94 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	static final int n = 9;
+	static int dp[] = new int[10];
+	
+	static boolean visited[];
+	static boolean selected[];
+	
+	static List<Integer> a;
+	static List<Integer> validB;
+	static List<Integer> b;
+	static int capacity;
+	
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        int in, win;
+        for(int t = 1; t <= T; t++) {
+        	capacity = 0;
+        	win = 0;
+        	a = new ArrayList<Integer>();
+        	st = new StringTokenizer(br.readLine());
+        	validB = new ArrayList<Integer>();
+        	visited = new boolean[n * 2 + 1];
+        	selected = new boolean[n];
+        	b = new ArrayList<Integer>();
+        	for(int i = 0; i < n; i++) {
+        		in = Integer.parseInt(st.nextToken());
+        		visited[in] = true;
+        		a.add(in);
+        		capacity += in;
+        	}
+        	Collections.sort(a, Collections.reverseOrder());
+        	for(int i = n * 2; i > 0; i--) {
+        		if(!visited[i]) {
+        			validB.add(i);
+        			b.add(i);
+        			capacity += i;
+        		}
+        	}
+        	int sp, k;
+        	for(int i = 0; i < n; i++) {
+        		b.set(0, validB.get(i));
+        		selected[i] = true;
+        		win += countWin(0, 0, 0);
+        		selected[i] = false;
+        	}
+        	sb.append("#").append(t).append(" ").append(win).append(" ").append(getFact(n) - win).append("\n");
+        }
+        
+        System.out.println(sb);
+        br.close();
+    }
+    
+    static int getFact(int num) {
+    	if(num <= 2) return 1 + num / 2;
+    	
+    	if(dp[num] > 0)
+    		return dp[num];
+    	
+    	return num * getFact(num - 1);
+    }
+
+    static int countWin(int idx, int aScore, int bScore) {
+    	int amount = a.get(idx) + b.get(idx);
+    	if (a.get(idx) > b.get(idx))
+    		aScore += amount;
+    	else
+    		bScore += amount;
+    	
+    	if(aScore > capacity / 2) {
+    		return getFact(n - idx - 1);
+    	}
+    	if(bScore > capacity / 2) {
+    		return 0;
+    	}
+
+    	int ret = 0;
+    	
+    	for(int i = 0; i < n; i++) {
+    		if(!selected[i]) {
+    			selected[i] = true;
+    			b.set(idx + 1, validB.get(i));
+    			ret += countWin(idx + 1, aScore, bScore);
+    			selected[i] = false;
+    		}
+    	}
+    	return ret;
+    }
+}
