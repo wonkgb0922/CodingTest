96a056d3a9c0d25b40416543c6527cc9c05ba6b5
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-02-04 10:05:27 +0900

    [Silver V] Title: 문서 검색, Time: 64 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -2,8 +2,6 @@ import java.io.*;
 import java.util.*;
  
 public class Main {
-	static List<Integer>[] map;
-	static boolean visited[];
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
