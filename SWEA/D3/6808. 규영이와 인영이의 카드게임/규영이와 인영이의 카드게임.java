4e50145adb427f0b738e2d7533807287c409349d
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-02-05 01:09:20 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 964 ms, Memory: 28,792 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -66,12 +66,10 @@ public class Solution {
         	else
         		bScore += amount;
         	
-        	if(aScore > capacity / 2) {
+        	if(aScore > capacity / 2)
         		return getFact(n - idx - 1);
-        	}
-        	if(bScore > capacity / 2) {
+        	if(bScore > capacity / 2)
         		return 0;
-        	}
     	}
     	int ret = 0;
     	for(int i = 0; i < n; i++) {
