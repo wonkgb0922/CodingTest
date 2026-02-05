6bf072539ed2d621f2f54ce4174eeb4dd73fcb37
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-02-05 23:10:42 +0900

    [D4] Title: 햄스터, Time: 498 ms, Memory: 58,544 KB -BaekjoonHub

---
+++ SWEA/D4/8275. 햄스터/README.md
@@ -0,0 +1,15 @@
+# [D4] 햄스터 - 8275 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWxQ310aOlQDFAWL) 
+
+### 성능 요약
+
+메모리: 58,544 KB, 시간: 498 ms, 코드길이: 1,884 Bytes
+
+### 제출 일자
+
+2026-02-05 23:10
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/8275. 햄스터/햄스터.java
@@ -0,0 +1,81 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+	static int res[];
+	static int ary[];
+	static int s[];
+	static int n, x;
+	static int tot;
+	static List<Record> l = new LinkedList<Record>();
+	
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        
+        for(int t = 1; t <= T; t++) {
+        	l.clear();
+        	st = new StringTokenizer(br.readLine());
+        	n = Integer.parseInt(st.nextToken());
+        	x = Integer.parseInt(st.nextToken());
+        	int m = Integer.parseInt(st.nextToken());
+        	ary = new int[n];
+        	res = new int[n];
+        	s = new int[n + 1];
+        	tot = -1;
+
+        	while(m-- > 0) {
+        		st = new StringTokenizer(br.readLine());
+        		l.add(new Record(Integer.parseInt(st.nextToken()) - 1,
+        				Integer.parseInt(st.nextToken()) - 1,
+        				Integer.parseInt(st.nextToken())));
+        	}
+        	dfs(0);
+        	sb.append("#").append(t).append(" ");
+        	if(tot == -1)
+            	sb.append("-1\n");
+            else {
+            	for(int i = 0; i < n; i++)
+            		sb.append(res[i]).append(" ");
+            	sb.append("\n");
+            }
+        }
+        
+        	
+        System.out.println(sb);
+    }
+	
+	static void dfs(int idx) {
+		if(idx == n) {
+			for(Record r : l) {
+				if(s[r.end + 1] - s[r.start] != r.s)
+					return;
+			}
+			if(tot < s[idx]) {
+				tot = s[idx];
+				res = Arrays.copyOf(ary, n);
+			}
+			return;
+		}
+		for(int i = 0; i <= x; i++) {
+			ary[idx] = i;
+			s[idx + 1] = s[idx] + i;
+			dfs(idx + 1);
+		}
+		
+	}
+}
+
+class Record {
+	int start;
+	int end;
+	int s;
+	Record(int start, int end, int s) {
+		this.start = start;
+		this.end = end;
+		this.s = s;
+	}
+}
