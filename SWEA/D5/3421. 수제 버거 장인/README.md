7677cc271fa086a3b1083ef00895f28b3a4df8e8
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-02-06 20:44:55 +0900

    [D5] Title: 수제 버거 장인, Time: 272 ms, Memory: 103,472 KB -BaekjoonHub

---
+++ SWEA/D5/3421. 수제 버거 장인/README.md
@@ -0,0 +1,15 @@
+# [D5] 수제 버거 장인 - 3421 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWErcQmKy6kDFAXi) 
+
+### 성능 요약
+
+메모리: 103,472 KB, 시간: 272 ms, 코드길이: 2,145 Bytes
+
+### 제출 일자
+
+2026-02-06 20:44
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D5/3421. 수제 버거 장인/수제 버거 장인.java
@@ -0,0 +1,85 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+	static int n, m;
+	static int res;
+	static int valid[];
+	static TreeSet<Pair> s = new TreeSet<Pair>();
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        int in1, in2;
+        for(int t = 1; t <= T; t++) {
+        	st = new StringTokenizer(br.readLine());
+        	n = Integer.parseInt(st.nextToken());
+        	m = Integer.parseInt(st.nextToken());
+        	res = 1;
+        	valid = new int[n];
+            s.clear();
+//        	s = new TreeSet<Pair>();
+        	while(m-- > 0) {
+        		st = new StringTokenizer(br.readLine());
+        		in1 = Integer.parseInt(st.nextToken()) - 1;
+        		in2 = Integer.parseInt(st.nextToken()) - 1;
+        		if(in1 > in2) {
+        			int temp = in1;
+        			in1 = in2;
+        			in2 = temp;
+        		}
+        		s.add(new Pair(in1, in2));
+        	}
+        	for(int i = 0; i < n; i++) {
+        		dfs(i);        		
+        	}
+        	sb.append("#").append(t).append(" ").append(res).append("\n");
+
+        }
+        System.out.println(sb);
+    }
+	
+	static void dfs(int idx) {
+		res++;
+		if(idx == n - 1) return;
+		Pair start = new Pair(idx, 0);
+		Pair end = new Pair(idx + 1, 0);
+		for(Pair p : s.subSet(start, end)) {
+			valid[p.second]++;
+		}
+		for(int i = idx + 1; i < n; i++) {
+			if(valid[i] == 0) {
+				dfs(i);
+			}
+		}
+		for(Pair p : s.subSet(start, end)) {
+			valid[p.second]--;
+		}
+	}
+}
+class Pair implements Comparable<Pair> {
+	int first;
+	int second;
+	Pair(int first, int second) {
+		this.first = first;
+		this.second = second;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		Pair p = (Pair) obj;
+		return this.first == p.first && this.second == p.second;
+	}
+	@Override
+	public int hashCode() {
+		return this.first * 1000 + this.second;
+	}
+	
+	public int compareTo(Pair o) {
+		int ret = Integer.compare(this.first, o.first);
+		return (ret != 0) ? ret : Integer.compare(this.second, o.second);
+	}
+
+}
