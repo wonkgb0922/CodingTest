593a239d6a781abdf13f0b883f70f4aca35191ed
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-02-19 15:37:37 +0900

    [D4] Title: 치즈 도둑, Time: 261 ms, Memory: 40,544 KB -BaekjoonHub

---
+++ SWEA/D4/7733. 치즈 도둑/README.md
@@ -0,0 +1,15 @@
+# [D4] 치즈 도둑 - 7733 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWrDOdQqRCUDFARG) 
+
+### 성능 요약
+
+메모리: 40,544 KB, 시간: 261 ms, 코드길이: 1,738 Bytes
+
+### 제출 일자
+
+2026-02-19 15:37
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+	static int ary[][];
+	static boolean visited[][];
+	static int dir[][] = {{0 ,1}, {0, -1}, {1, 0}, {-1, 0}};
+	static int n;
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        int max, res, cnt;
+        for(int t = 1; t <= T; t++) {
+        	n = Integer.parseInt(br.readLine());
+        	ary = new int[n][n];
+        	max = 0;
+        	res = 0;
+        	for(int i = 0; i < n; i++) {
+        		st = new StringTokenizer(br.readLine());
+        		for(int j = 0; j < n; j++) {
+        			ary[i][j] = Integer.parseInt(st.nextToken());
+        			max = Math.max(ary[i][j], max);
+        		}
+        	}
+        	for(int k = 0; k < max; k++) {
+        		visited = new boolean[n][n];
+        		cnt = 0;
+        		for(int i = 0; i < n; i++) {
+            		for(int j = 0; j < n; j++) {
+            			if(!visited[i][j]) {
+                			visited[i][j] = true;
+                			if(ary[i][j] > k) {
+                				dfs(i, j, k);
+                				cnt++;
+                			}
+            			}
+            		}
+            	}
+        		res = Math.max(res, cnt);
+        	}
+        	sb.append(String.format("#%d %d\n", t, res));
+        }
+        System.out.println(sb);
+    }
+	static void dfs(int i, int j, int k) {
+		for(int d = 0; d < 4; d++) {
+			int ii = i + dir[d][0];
+			int jj = j + dir[d][1];
+			if(ii >= 0 && ii < n && jj >= 0 && jj < n) {
+				if(!visited[ii][jj] && ary[ii][jj] > k) {
+					visited[ii][jj] = true;
+					dfs(ii, jj, k);
+				}
+			}
+		}
+	}
+}
