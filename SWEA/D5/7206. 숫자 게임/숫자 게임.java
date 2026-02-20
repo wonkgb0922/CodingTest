ec7810432f827060663abc86be687218b5eb4f72
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-02-20 23:35:45 +0900

    [D5] Title: 숫자 게임, Time: 94 ms, Memory: 25,216 KB -BaekjoonHub

--- SWEA/D5/7206. 숫자 게임/숫자 게임.java
+++ SWEA/D5/7206. 숫자 게임/숫자 게임.java
@@ -20,19 +20,15 @@ class Solution
     	if(comp) {
     		if(idx < 10) return 0;
     		if(dp[idx] > 0) return dp[idx];
-    		for(int i = 10; idx / i > 0; i *= 10) {
+    		for(int i = 10; idx / i > 0; i *= 10)
     			dp[idx] = Math.max(dp[idx], dfs(idx, idx / i, idx % i, false));
-    		}
     	}
     	else {
-    		for(int i = 10; prev / i > 0; i *= 10) {
+    		for(int i = 10; prev / i > 0; i *= 10)
     			dp[idx] = Math.max(dp[idx], dfs(idx, prev / i, cur * (prev % i), false));
-    		}
+    		
 			dp[idx] = Math.max(dp[idx], dfs(prev * cur, 1, 1, true) + 1);
     	}
-		
 		return dp[idx];
-    	
     }
-    
 }
