892df80517a4163eb308067202e70ef168611a03
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-03-09 21:27:15 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 190 ms, Memory: 43,072 KB -BaekjoonHub

---
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/README.md
@@ -0,0 +1,15 @@
+# [D4] 파핑파핑 지뢰찾기 - 1868 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5LwsHaD1MDFAXc) 
+
+### 성능 요약
+
+메모리: 43,072 KB, 시간: 190 ms, 코드길이: 1,892 Bytes
+
+### 제출 일자
+
+2026-03-09 21:27
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
@@ -0,0 +1,76 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+	static int p[];
+	static int dir[][] = {{1, 0}, {1, -1}, {1, 1}, {0, 1}, {0, -1}, {-1, 0}, {-1, 1}, {-1, -1}};
+	static int n, m, res, cnt;
+	static boolean isZero[][];
+	static char ary[][];
+	static int find(int v) {
+		if(p[v] < 0) return v;
+		return p[v] = find(p[v]);
+	}
+	static void merge(int a, int b) {
+		a = find(a);
+		b = find(b);
+		if(a == b) return;
+		res--;
+		p[b] += p[a];
+		p[a] = b;
+	}
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		int ii, jj;
+		for(int tc = 1; tc <= T; tc++) {
+			n = Integer.parseInt(br.readLine());
+			ary = new char[n][n];
+			isZero = new boolean[n][n];
+			p = new int[n * n];
+			res = n * n;
+			cnt = 0;
+			for(int i = 0; i < n; i++)
+				ary[i] = br.readLine().toCharArray();
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < n; j++) {
+					p[i * n + j] = -1;
+					if(ary[i][j] == '*')
+						cnt++;
+					else
+						Zero(i, j);
+				}
+			}
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < n; j++) {
+					if(!isZero[i][j] | ary[i][j] == '*') continue;
+					for(int d = 0; d < 8; d++) {
+						ii = i + dir[d][0];
+						jj = j + dir[d][1];
+						if(ii >= 0 && ii < n && jj >= 0 && jj < n) {
+							if(ary[ii][jj] == '*' || (!isZero[ii][jj] && p[find(ii * n + jj)] < -1)) continue;
+							merge(i * n + j, ii * n + jj);
+						}
+					}
+				}
+			}
+			sb.append(String.format("#%d %d\n", tc, res-cnt));
+		}
+		System.out.print(sb);
+	}
+	static void Zero(int i, int j) {
+		int ii, jj;
+		for(int d = 0; d < 8; d++) {
+			ii = i + dir[d][0];
+			jj = j + dir[d][1];
+			if(ii >= 0 && ii < n && jj >= 0 && jj < n) {
+				if(ary[ii][jj] == '*') {
+					isZero[i][j] = false;
+					return;
+				}
+			}
+		}
+		isZero[i][j] = true;
+	}
+}
