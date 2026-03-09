c8d275732c41ae1098e69961a06711b7b1eb6563
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-03-09 10:19:29 +0900

    [D4] Title: 사탕 가방, Time: 119 ms, Memory: 29,312 KB -BaekjoonHub

---
+++ SWEA/D4/11446. 사탕 가방/README.md
@@ -0,0 +1,15 @@
+# [D4] 사탕 가방 - 11446 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXdHxTNqC2IDFAS5) 
+
+### 성능 요약
+
+메모리: 29,312 KB, 시간: 119 ms, 코드길이: 1,045 Bytes
+
+### 제출 일자
+
+2026-03-09 10:19
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/11446. 사탕 가방/사탕 가방.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		int n;
+		long m, l, r, mid, sum, res = 0;
+		long ary[];
+		for(int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			m = Long.parseLong(st.nextToken());
+			st = new StringTokenizer(br.readLine());
+			ary = new long[n];
+			for(int i = 0; i < n; i++)
+				ary[i] = Long.parseLong(st.nextToken());
+			l = 0;
+			r = 1000000000000000001L;
+			while(l < r) {
+				mid = (l + r) / 2 + 1;
+				if(mid == 0) break;
+				sum = 0;
+				for(int i = 0; i < n; i++)
+					sum += ary[i] / mid;
+				if(m <= sum) {
+					l = mid;
+					res = l;
+				}
+				else if(m > sum) {
+					r = mid - 1;
+					res = r;
+				}
+			}
+			sb.append(String.format("#%d %d\n", tc, res));
+		}
+		System.out.println(sb);
+	}
+}
