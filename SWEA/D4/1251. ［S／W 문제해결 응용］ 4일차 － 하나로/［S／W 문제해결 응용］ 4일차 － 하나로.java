279b538f99f908ffc93a36d52284b40fcb42ebf7
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-03-10 16:35:41 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 237 ms, Memory: 81,256 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -2,22 +2,11 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-	static int n;
+	static int n, cnt;
 	static double e;
-	static int p[];
+	static boolean visited[];
 	static long[] x, y;
 	
-	static int find(int a) {
-		if(p[a] < 0) return a;
-		return p[a] = find(p[a]);
-	}
-	static void merge(int a, int b) {
-		a = find(a);
-		b = find(b);
-		if(a == b) return;
-		p[b] = a;
-		p[a]--;
-	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -28,16 +17,15 @@ public class Solution {
 		PriorityQueue<Edge> pq;
 		for(int tc = 1; tc <= T; tc++) {
 			n = Integer.parseInt(br.readLine());
-			p = new int[n];
+			visited = new boolean[n];
 			x = new long[n];
 			y = new long[n];
 			pq = new PriorityQueue<>();
 			res = 0;
 			st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < n; i++) {
-				p[i] = -1;
+			for(int i = 0; i < n; i++)
 				x[i] = Integer.parseInt(st.nextToken());
-			}
+			
 			st = new StringTokenizer(br.readLine());
 			for(int i = 0; i < n; i++) {
 				y[i] = Integer.parseInt(st.nextToken());
@@ -45,18 +33,21 @@ public class Solution {
 					pq.offer(new Edge((x[0] - x[i]) * (x[0] - x[i]) + 
 							(y[0] - y[i]) * (y[0] - y[i]), i));
 			}
+			visited[0] = true;
 			e = Double.parseDouble(br.readLine());
+			cnt = 1;
 			while(!pq.isEmpty()) {
 				Edge edge = pq.poll();
-				if(find(edge.dest) == find(0)) continue;
-				merge(0, edge.dest);
+				if(visited[edge.dest]) continue;
+				visited[edge.dest] = true;
+				cnt++;
 				res += edge.w;
 				for(int i = 1; i < n; i++) {
-					if(find(i) == find(edge.dest)) continue;
+					if(visited[i]) continue;
 					pq.offer(new Edge((x[edge.dest] - x[i]) * (x[edge.dest] - x[i]) +
 							(y[edge.dest] - y[i]) * (y[edge.dest] - y[i]), i));
 				}
-				if(p[0] == -1 * n) break;
+				if(cnt == n) break;
 			}
 			sb.append(String.format("#%d %d\n", tc, Math.round(res * e)));
 		}
