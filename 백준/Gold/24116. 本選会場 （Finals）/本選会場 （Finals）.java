c13c66cd8c53c09e1dbe612beb7b1d8237324e42
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-03-19 11:50:32 +0900

    [Gold II] Title: 本選会場 (Finals), Time: 420 ms, Memory: 48648 KB -BaekjoonHub

---
+++ 백준/Gold/24116. 本選会場 （Finals）/本選会場 （Finals）.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int p[];
+	static int n, m, k 	;
+	
+	static int find(int v) {
+		if(p[v] == 0) return v;
+		return p[v] = find(p[v]);
+	}
+	
+	static void merge(int a, int b) {
+		a = find(a);
+		b = find(b);
+		if(a == b) return;
+		p[a] = b;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		p = new int[n + 1];
+		int res = 0;
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		while(m-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			pq.offer(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+		}
+		int cnt = n;
+		while(!pq.isEmpty()) {
+			if(cnt == k) break;
+			Node node = pq.poll();
+			if(find(node.s) == find(node.e)) continue;
+			merge(node.s, node.e);
+			cnt--;
+			res += node.w;
+		}
+		System.out.println(res);
+	}
+}
+
+class Node implements Comparable<Node> {
+	int s;
+	int e;
+	int w;
+	
+	public Node(int s, int e, int w) {
+		this.s = s;
+		this.e = e;
+		this.w = w;
+	}
+
+	@Override
+	public int compareTo(Node o) {
+		return Integer.compare(this.w, o.w);
+	}
+}
