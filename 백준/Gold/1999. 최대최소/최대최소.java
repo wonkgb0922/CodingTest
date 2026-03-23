099705b5d3c74d45ee72b44c5b4ccb158ba60f65
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-03-23 16:29:44 +0900

    [Gold IV] Title: 최대최소, Time: 424 ms, Memory: 46832 KB -BaekjoonHub

--- 백준/Gold/1999. 최대최소/최대최소.java
+++ 백준/Gold/1999. 최대최소/최대최소.java
@@ -14,13 +14,13 @@ public class Main {
 		max = new int[n][n];
 		min = new int[n][n];
 		int a, b;
-		LinkedList<Node> maxdq, mindq;
-		maxdq = new LinkedList<>();
-		mindq = new LinkedList<>();
+		ArrayDeque<Node> maxdq, mindq;
+		maxdq = new ArrayDeque<>();
+		mindq = new ArrayDeque<>();
 		for(int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			maxdq = new LinkedList<>();
-			mindq = new LinkedList<>();
+			maxdq = new ArrayDeque<>();
+			mindq = new ArrayDeque<>();
 			for(int j = 0; j < n; j++) {
 				a = Integer.parseInt(st.nextToken());
 				while(!maxdq.isEmpty() && maxdq.peek().j + m <= j) maxdq.poll();
@@ -61,8 +61,8 @@ public class Main {
 			}
 		}
 		for(int j = 0; j < n; j++) {
-			maxdq = new LinkedList<>();
-			mindq = new LinkedList<>();
+			maxdq = new ArrayDeque<>();
+			mindq = new ArrayDeque<>();
 			for(int i = 0; i < n; i++) {
 				while(!maxdq.isEmpty() && maxdq.peek().i + m <= i) maxdq.poll();
 				while(!mindq.isEmpty() && mindq.peek().i + m <= i) mindq.poll();
