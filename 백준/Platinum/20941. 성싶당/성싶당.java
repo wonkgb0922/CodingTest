f1d3306fc1e78deb87deb541e20aedf91ee4a400
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-03-27 09:37:30 +0900

    [Platinum V] Title: 성싶당, Time: 632 ms, Memory: 274948 KB -BaekjoonHub

--- 백준/Platinum/20941. 성싶당/성싶당.java
+++ 백준/Platinum/20941. 성싶당/성싶당.java
@@ -9,36 +9,34 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         n = Integer.parseInt(br.readLine());
-        int cnt = 0;
+        int idx;
         visited = new boolean[1 << n];
-//        sb = new StringBuilder(1 << n);
-        String in = br.readLine();
-        int p = 0;
-        for(int i = 0; i < n; i++) {
-        	p += (in.charAt(i) == '1') ? (1 << (n - i - 1)) : 0;
-        }
-        while(true) {
-        	visited[p] = true;
-    		for(int i = 32 - Integer.numberOfLeadingZeros(p); i < n; i++)
-    			sb.append(0);
-    		if(p > 0)
-    			sb.append(Integer.toBinaryString(p));
-    		sb.append("\n");
-    		p = ~p;
-    		p &= ((1 << n) - 1);
-    		int idx = 0;
-    		int next = p;
-    		while(visited[next] && idx < n) {
-    			if(((p >> idx) & 1) > 0)
-    				next = p & ~(1 << idx);
-    			else
-    				next = p | (1 << idx);
-    			next &= ((1 << n) - 1);
-    			idx++;
-    		}
 
-    		if(idx >= n) break;
-    		p = next;
+		int p1 = Integer.parseInt(br.readLine(), 2);
+		int pn = p1;
+		int rpn, next = 0;
+        while(true) {
+        	rpn = (~pn) & ((1 << n) - 1);
+        	visited[pn] = true;
+        	visited[rpn] = true;
+    		for(int i = 32 - Integer.numberOfLeadingZeros(pn); i < n; i++)
+    			sb.append(0);
+    		if(pn > 0)
+    			sb.append(Integer.toBinaryString(pn));
+    		sb.append("\n");
+    		for(int i = 32 - Integer.numberOfLeadingZeros(rpn); i < n; i++)
+    			sb.append(0);
+    		if(rpn > 0)
+    			sb.append(Integer.toBinaryString(rpn));
+    		sb.append("\n");
+    		idx = 0;
+    		while(idx < n) {
+        		next = pn ^ (1 << idx);
+        		if(!visited[next]) break;
+        		idx++;
+    		}
+    		if(idx == n) break;
+    		pn = next;
         }
 		System.out.print(sb);
     }
