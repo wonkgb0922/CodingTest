f5466e291dc21e394dce616ca175608d4dfde746
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-04-09 00:18:12 +0900

    [D3] Title: 0/1 Knapsack, Time: 114 ms, Memory: 30,848 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -1,39 +1,38 @@
 import java.util.*;
 import java.io.*;
- 
-class Solution
-{
-	static int n;
-	static int k;
-	static int dp[][];
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        int T = Integer.parseInt(br.readLine());
-        int v, c;
-        for(int t = 1; t <= T; t++) {
-        	st = new StringTokenizer(br.readLine());
-        	n = Integer.parseInt(st.nextToken());
-        	k = Integer.parseInt(st.nextToken());
-        	dp = new int[n][k + 1];
-    		st = new StringTokenizer(br.readLine());
-			v = Integer.parseInt(st.nextToken());
-			c = Integer.parseInt(st.nextToken());
-        	for(int i = v; i <= k; i++)
-    			dp[0][i] = c;
-        	for(int i = 1; i < n; i++) {
-        		st = new StringTokenizer(br.readLine());
-    			v = Integer.parseInt(st.nextToken());
-    			c = Integer.parseInt(st.nextToken());
-        		for(int j = 1; j <= k; j++) {
-        			dp[i][j] = dp[i - 1][j];
-        			if(j - v >= 0)
-        				dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - v] + c);
-        		}
-        	}
-        	sb.append(String.format("#%d %d\n", t, dp[n - 1][k]));
-        }
-        System.out.println(sb);
-    }
+
+public class Solution {
+	static int n, k;
+	static int c[], v[], dp[][];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		for(int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			k = Integer.parseInt(st.nextToken());
+			c = new int[n];
+			v = new int[n];
+			dp = new int[n][k + 1];
+			for(int i = 0; i < n; i++) {
+				st = new StringTokenizer(br.readLine());
+				v[i] = Integer.parseInt(st.nextToken());
+				c[i] = Integer.parseInt(st.nextToken());
+			}
+			sb.append("#").append(tc).append(" ").append(sol(0, 0)).append("\n");
+		}
+		System.out.print(sb);
+	}
+	
+	static int sol(int idx, int w) {
+		if(w > k) return -2000000000;
+		if(idx >= n) return 0;
+		if(dp[idx][w] > 0) return dp[idx][w];
+		
+		dp[idx][w] = Math.max(dp[idx][w], sol(idx + 1, w));
+		dp[idx][w] = Math.max(dp[idx][w], sol(idx + 1, w + v[idx]) + c[idx]);
+		return dp[idx][w];
+	}
 }
