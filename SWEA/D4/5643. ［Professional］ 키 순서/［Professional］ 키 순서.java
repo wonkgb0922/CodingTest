b16e454fa9e54547d56d93d039dc634c23b18f71
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-04-15 08:35:27 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,040 ms, Memory: 100,344 KB -BaekjoonHub

---
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	static final int INF = 1000000000;
+	static int n, m;
+	static int dis[][];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        int v, u;
+        
+        for(int tc = 1; tc <= T; tc++) {
+        	n = Integer.parseInt(br.readLine());
+        	m = Integer.parseInt(br.readLine());
+        	dis = new int[n + 1][n + 1];
+        	for(int i = 1; i <= n; i++) {
+        		for(int j = 1; j <= n; j++) {
+        			if(i == j) continue;
+        			dis[i][j] = INF;
+        		}
+        	}
+        	while(m-- > 0) {
+        		st = new StringTokenizer(br.readLine());
+        		v = Integer.parseInt(st.nextToken());
+        		u = Integer.parseInt(st.nextToken());
+        		dis[v][u] = 1;
+        	}
+        	for(int k = 1; k <= n; k++) {
+        		for(int i = 1; i <= n; i++) {
+        			for(int j = 1; j <= n; j++)
+        				dis[i][j] = Math.min(dis[i][j], dis[i][k] + dis[k][j]);
+        		}
+        	}
+        	int cnt, res = 0;
+        	for(int i = 1; i <= n; i++) {
+        		cnt = 1;
+        		for(int j = 1; j <= n; j++) {
+        			// 자신에게서 뻗는 개수와 자신으로 향하는 개수를 찾으면 됨.
+        			if(i == j) continue;
+        			if(dis[i][j] < INF)
+        				cnt++;
+        			if(dis[j][i] < INF)
+        				cnt++;
+        		}
+        		if(cnt == n)
+        			res++;
+        	}
+        	sb.append("#").append(tc).append(" ").append(res).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
