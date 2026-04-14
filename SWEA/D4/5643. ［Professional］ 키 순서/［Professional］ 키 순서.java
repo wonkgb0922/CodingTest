0270d5865e23e7c3fcfc24f58d910c93d5d2fda2
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-04-15 08:59:51 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,470 ms, Memory: 99,328 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Solution {
 	static final int INF = 1000000000;
 	static int n, m;
-	static int dis[][];
+	static boolean dis[][];
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -15,23 +15,20 @@ public class Solution {
         for(int tc = 1; tc <= T; tc++) {
         	n = Integer.parseInt(br.readLine());
         	m = Integer.parseInt(br.readLine());
-        	dis = new int[n + 1][n + 1];
-        	for(int i = 1; i <= n; i++) {
-        		for(int j = 1; j <= n; j++) {
-        			if(i == j) continue;
-        			dis[i][j] = INF;
-        		}
-        	}
+        	dis = new boolean[n + 1][n + 1];
+        	for(int i = 1; i <= n; i++)
+        		dis[i][i] = true;
+        	
         	while(m-- > 0) {
         		st = new StringTokenizer(br.readLine());
         		v = Integer.parseInt(st.nextToken());
         		u = Integer.parseInt(st.nextToken());
-        		dis[v][u] = 1;
+        		dis[v][u] = true;
         	}
         	for(int k = 1; k <= n; k++) {
         		for(int i = 1; i <= n; i++) {
         			for(int j = 1; j <= n; j++)
-        				dis[i][j] = Math.min(dis[i][j], dis[i][k] + dis[k][j]);
+        				dis[i][j] = dis[i][j] | (dis[i][k] && dis[k][j]);
         		}
         	}
         	int cnt, res = 0;
@@ -40,9 +37,7 @@ public class Solution {
         		for(int j = 1; j <= n; j++) {
         			// 자신에게서 뻗는 개수와 자신으로 향하는 개수를 찾으면 됨.
         			if(i == j) continue;
-        			if(dis[i][j] < INF)
-        				cnt++;
-        			if(dis[j][i] < INF)
+        			if(dis[i][j] | dis[j][i])
         				cnt++;
         		}
         		if(cnt == n)
