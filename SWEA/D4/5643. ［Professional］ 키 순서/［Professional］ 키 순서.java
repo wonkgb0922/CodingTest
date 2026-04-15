1d76802665a51c8d0d4d76af5339dcd49d2fb902
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-04-15 09:31:29 +0900

    [D4] Title: [Professional] 키 순서, Time: 976 ms, Memory: 100,636 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -24,7 +24,7 @@ public class Solution {
         	for(int k = 1; k <= n; k++) {
         		for(int i = 1; i <= n; i++) {
         			for(int j = 1; j <= n; j++)
-        				dis[i][j] = (dis[i][j] & 1) | ((dis[i][k] & 1) & dis[k][j] & 1);
+        				dis[i][j] = dis[i][j] | (dis[i][k] & dis[k][j]);
         		}
         	}
         	int cnt, res = 0;
@@ -33,7 +33,7 @@ public class Solution {
         		for(int j = 1; j <= n; j++) {
         			// 자신에게서 뻗는 개수와 자신으로 향하는 개수를 찾으면 됨.
         			if(i == j) continue;
-        			if((dis[i][j] & 1) > 0 || (dis[j][i] & 1) > 0)
+        			if((dis[i][j] | dis[j][i]) > 0)
         				cnt++;
         		}
         		if(cnt == n)
