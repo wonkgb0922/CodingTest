f66a2946296fbe859f59cc5c3a0669a92bb3df0d
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-04-15 09:35:49 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,827 ms, Memory: 99,512 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -1,42 +1,48 @@
 import java.io.*;
 import java.util.*;
-
+ 
 public class Solution {
-	static int n, m;
-	static int dis[][];
+    static int n, m;
+    static boolean dis[][];
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
-        
+        int v, u;
+         
         for(int tc = 1; tc <= T; tc++) {
-        	n = Integer.parseInt(br.readLine());
-        	m = Integer.parseInt(br.readLine());
-        	dis = new int[n + 1][n + 1];
-        	while(m-- > 0) {
-        		st = new StringTokenizer(br.readLine());
-        		dis[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = 1;
-        	}
-        	for(int k = 1; k <= n; k++) {
-        		for(int i = 1; i <= n; i++) {
-        			for(int j = 1; j <= n; j++)
-        				dis[i][j] = dis[i][j] | (dis[i][k] & dis[k][j]);
-        		}
-        	}
-        	int cnt, res = 0;
-        	for(int i = 1; i <= n; i++) {
-        		cnt = 1;
-        		for(int j = 1; j <= n; j++) {
-        			// 자신에게서 뻗는 개수와 자신으로 향하는 개수를 찾으면 됨.
-        			if(i == j) continue;
-        			if((dis[i][j] | dis[j][i]) > 0)
-        				cnt++;
-        		}
-        		if(cnt == n)
-        			res++;
-        	}
-        	sb.append("#").append(tc).append(" ").append(res).append("\n");
+            n = Integer.parseInt(br.readLine());
+            m = Integer.parseInt(br.readLine());
+            dis = new boolean[n + 1][n + 1];
+            for(int i = 1; i <= n; i++)
+                dis[i][i] = true;
+             
+            while(m-- > 0) {
+                st = new StringTokenizer(br.readLine());
+                v = Integer.parseInt(st.nextToken());
+                u = Integer.parseInt(st.nextToken());
+                dis[v][u] = true;
+            }
+            for(int k = 1; k <= n; k++) {
+                for(int i = 1; i <= n; i++) {
+                    for(int j = 1; j <= n; j++)
+                        dis[i][j] = dis[i][j] || (dis[i][k] && dis[k][j]);
+                }
+            }
+            int cnt, res = 0;
+            for(int i = 1; i <= n; i++) {
+                cnt = 1;
+                for(int j = 1; j <= n; j++) {
+                    // 자신에게서 뻗는 개수와 자신으로 향하는 개수를 찾으면 됨.
+                    if(i == j) continue;
+                    if(dis[i][j] || dis[j][i])
+                        cnt++;
+                }
+                if(cnt == n)
+                    res++;
+            }
+            sb.append("#").append(tc).append(" ").append(res).append("\n");
         }
         System.out.print(sb);
     }
