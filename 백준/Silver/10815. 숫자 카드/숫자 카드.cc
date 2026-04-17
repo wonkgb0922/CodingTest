610243f8c64c48f74684e4f0b4522112d1fa229c
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-04-17 14:57:52 +0900

    [Silver V] Title: 숫자 카드, Time: 596 ms, Memory: 25388 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -1,11 +1,6 @@
 #include <stdio.h>
-#include <string>
-#include <string.h>
 #include <iostream>
-#include <cmath>
-#include <vector>
 #include <algorithm>
-#include <map>
 #include <set>
 
 using namespace std;
@@ -14,19 +9,14 @@ int main(void){
     int n, m, input, res;
     set<int> s;
     scanf("%d", &n);
-    while(n--)
-    {
+    while(n--) {
         scanf("%d", &input);
         s.insert(input);
     }
     scanf("%d", &m);
-    while(m--)
-    {
+    while(m--) {
         scanf("%d", &input);
         printf("%d ", s.count(input));
     }
-    
-
-	return 0;
-    
+    return 0;
 }
