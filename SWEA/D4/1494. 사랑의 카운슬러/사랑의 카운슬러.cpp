fb4f89f8f2ca02679ea9ad85b8a40e03f339011c
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-04-19 05:37:31 +0900

    [D4] Title: 사랑의 카운슬러, Time: 35 ms, Memory: 5,988 KB -BaekjoonHub

--- SWEA/D4/1494. 사랑의 카운슬러/사랑의 카운슬러.cpp
+++ SWEA/D4/1494. 사랑의 카운슬러/사랑의 카운슬러.cpp
@@ -7,7 +7,7 @@ int ary[20][2];
 int n, fn;
 long long res;
 
-void sol(int idx, int state, int cnt, long long f, long long s) {
+void sol(int idx, int cnt, long long f, long long s) {
 	if (idx + fn - cnt > n) return;
 	if (idx == n) {
 		long long temp = f * f + s * s;
@@ -15,9 +15,9 @@ void sol(int idx, int state, int cnt, long long f, long long s) {
 		return;
 	}
 
-	sol(idx + 1, state, cnt, f - ary[idx][0], s - ary[idx][1]);
-	if(cnt < fn)
-		sol(idx + 1, state | (1 << idx), cnt + 1, f + ary[idx][0], s + ary[idx][1]);
+	sol(idx + 1, cnt, f - ary[idx][0], s - ary[idx][1]);
+	if (cnt < fn)
+		sol(idx + 1, cnt + 1,  f + ary[idx][0], s + ary[idx][1]);
 }
 
 int main()
@@ -35,8 +35,8 @@ int main()
 		for (int i = 0; i < n; i++) {
 			cin >> ary[i][0] >> ary[i][1];
 		}
-		sol(1, 1, 1, ary[0][0], ary[0][1]);
+		sol(1, 1, ary[0][0], ary[0][1]);
 		cout << "#" << tc << " " << res << "\n";
-	}	
+	}
 	return 0;
 }
