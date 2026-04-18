9e25ab82f75d662447b53307d6f3d3213f111273
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-04-19 05:21:11 +0900

    [D4] Title: 사랑의 카운슬러, Time: 49 ms, Memory: 5,988 KB -BaekjoonHub

--- SWEA/D4/1494. 사랑의 카운슬러/사랑의 카운슬러.cpp
+++ SWEA/D4/1494. 사랑의 카운슬러/사랑의 카운슬러.cpp
@@ -7,31 +7,20 @@ int ary[20][2];
 int n, fn;
 long long res;
 
-void sol(int idx, int state, int cnt) {
+void sol(int idx, int state, int cnt, long long f, long long s) {
 	if (idx + fn - cnt > n) return;
 	if (idx == n) {
-		long long f = 0, s = 0, temp;
-		for (int i = 0; i < n; i++) {
-			if (state & (1 << i)) {
-				f += ary[i][0];
-				s += ary[i][1];
-			}
-			else {
-				f -= ary[i][0];
-				s -= ary[i][1];
-			}
-		}
-		temp = f * f + s * s;
+		long long temp = f * f + s * s;
 		res = min(res, temp);
 		return;
 	}
 
-	sol(idx + 1, state, cnt);
+	sol(idx + 1, state, cnt, f - ary[idx][0], s - ary[idx][1]);
 	if(cnt < fn)
-		sol(idx + 1, state | (1 << idx), cnt + 1);
+		sol(idx + 1, state | (1 << idx), cnt + 1, f + ary[idx][0], s + ary[idx][1]);
 }
 
-int main(void)
+int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -46,8 +35,8 @@ int main(void)
 		for (int i = 0; i < n; i++) {
 			cin >> ary[i][0] >> ary[i][1];
 		}
-		sol(1, 1, 1);
+		sol(1, 1, 1, ary[0][0], ary[0][1]);
 		cout << "#" << tc << " " << res << "\n";
-	}
+	}	
 	return 0;
 }
