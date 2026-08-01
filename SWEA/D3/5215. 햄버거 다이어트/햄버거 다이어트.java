f6523a908704421f54d9b27959861e2a400816cc
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-08-01 15:29:15 +0900

    [D3] Title: 햄버거 다이어트, Time: 96 ms, Memory: 31,332 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,40 +1,37 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class Solution
-{
-	static int n;
-	static int l;
-	static int dp[][];
-	public static void main(String[] args) throws Exception {
+public class Solution {
+    static int dp[][];
+    static int ary[][];
+    static int n, l;
+
+    static int sol(int idx, int cur) {
+        if(idx >= n) return 0;
+        if(dp[idx][cur] > 0) return dp[idx][cur];
+        dp[idx][cur] = sol(idx + 1, cur);
+        if(cur + ary[idx][1] <= l)
+            dp[idx][cur] = Math.max(dp[idx][cur], sol(idx + 1, cur + ary[idx][1]) + ary[idx][0]);
+        return dp[idx][cur];
+    }
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
-        int s[] = new int[20], k[] = new int[20];
-        
-    	dp = new int[21][10001];
-        for(int t = 1; t <= T; t++) {
-        	st = new StringTokenizer(br.readLine());
-        	n = Integer.parseInt(st.nextToken());
-        	l = Integer.parseInt(st.nextToken());
-        	for(int i = 1; i <= n; i++) {
-        		for(int j = 0; j <= l; j++)
-        			dp[i][j] = 0;
-        	}
-        	for(int i = 0; i < n; i++) {
-        		st = new StringTokenizer(br.readLine());
-        		s[i] = Integer.parseInt(st.nextToken());
-        		k[i] = Integer.parseInt(st.nextToken());
-        	}
-        	for(int i = 1; i <= n; i++) {
-        		for(int j = 0; j <= l; j++) {
-        			dp[i][j] = dp[i - 1][j];
-        			if(j >= k[i - 1])
-        				dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - k[i - 1]] + s[i - 1]);
-        		}
-        	}
-        	sb.append('#').append(t).append(' ').append(dp[n][l]).append('\n');
+        for (int t = 1; t <= T; t++) {
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            l = Integer.parseInt(st.nextToken());
+            dp = new int[n][l + 1];
+            ary = new int[n][2];
+            for(int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                ary[i][0] = Integer.parseInt(st.nextToken());
+                ary[i][1] = Integer.parseInt(st.nextToken());
+            }
+            sb.append("#").append(t).append(" ").append(sol(0, 0)).append("\n");
         }
         System.out.println(sb);
     }
