8f328a17d52e6377c841ecd638e7824fbdcd9995
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-08-01 17:06:59 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 97 ms, Memory: 28,632 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -1,46 +1,59 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Solution {
-	static int n;
-	static int x[], y[];
-	static int res, state;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine());
-		for(int tc = 1; tc <= T; tc++) {
-			n = Integer.parseInt(br.readLine());
-			x = new int[n + 2];
-			y = new int[n + 2];
-			st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < n + 2; i++) {
-				x[i] = Integer.parseInt(st.nextToken());
-				y[i] = Integer.parseInt(st.nextToken());
-			}
-			state = 1;
-			res = Integer.MAX_VALUE;
-			dfs(0, 0);
-			sb.append(String.format("#%d %d\n", tc, res));
-		}
-		System.out.print(sb);
-	}
-	static void dfs(int idx, int dis) {
-		if(Integer.bitCount(state) == n + 1) {
-			res = Math.min(res, dis + getDis(idx, 1));
-			return;
-		}
-		if(dis >= res) return;
-		for(int i = 2; i < n + 2; i++) {
-			if((state & (1 << i)) == 0) {
-				state ^= (1 << i);
-				dfs(i, dis + getDis(idx, i));
-				state ^= (1 << i);
-			}
-		}
-	}
-	static int getDis(int v, int u) {
-		return Math.abs(x[v] - x[u]) + Math.abs(y[v] - y[u]);
-	}
+    static int dp[][][];
+    static Pos ary[];
+    static Pos start, end;
+    static int n;
+    static final int INF = 1000000000;
+
+    static int sol(int idx, int prev, int state) {
+        if(idx >= n) return getDis(ary[prev], end);
+        if(dp[idx][prev][state] < INF) return dp[idx][prev][state];
+        for(int i = 1; i <= n; i++) {
+            if((state & (1 << i)) > 0) continue;
+            dp[idx][prev][state] = Math.min(dp[idx][prev][state],
+                    sol(idx + 1, i, state | (1 << i)) + getDis(ary[prev], ary[i]));
+        }
+        return dp[idx][prev][state];
+    }
+
+    static int getDis(Pos p1, Pos p2) {
+        return Math.abs(p1.i - p2.i) + Math.abs(p1.j - p2.j);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        dp = new int[11][11][1 << 11];
+        ary = new Pos[11];
+        for (int t = 1; t <= T; t++) {
+            n = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            ary[0] = new Pos(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            end = new Pos(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            for(int i = 0; i < n; i++) {
+                ary[i + 1] = new Pos(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            }
+            for(int i = 0; i <= n; i++) {
+                for(int j = 0; j <= n; j++)
+                    Arrays.fill(dp[i][j], INF);
+            }
+//            Arrays.fill(dp[n], INF);
+            sb.append("#").append(t).append(" ").append(sol(0, 0, 1)).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
+
+class Pos {
+    public Pos(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+    public int i;
+    public int j;
 }
