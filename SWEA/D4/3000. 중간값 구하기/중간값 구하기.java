74c1d0bef1cab891addb705ea93d7379aa2eda7d
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-08-16 01:03:01 +0900

    [D4] Title: 중간값 구하기, Time: 909 ms, Memory: 122,096 KB -BaekjoonHub

--- SWEA/D4/3000. 중간값 구하기/중간값 구하기.java
+++ SWEA/D4/3000. 중간값 구하기/중간값 구하기.java
@@ -1,50 +1,39 @@
-import java.util.*;
 import java.io.*;
- 
-class Solution
-{
-	static int n;
-    public static void main(String[] args) throws Exception {
+import java.util.*;
+
+public class Solution {
+    static int n;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
         int T = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> min = new PriorityQueue<>();
-        PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
-        int x, y, res;
-        for(int t = 1; t <= T; t++) {
-        	st = new StringTokenizer(br.readLine());
-        	n = Integer.parseInt(st.nextToken());
-        	x = Integer.parseInt(st.nextToken());
-        	res = 0;
-        	min.clear();
-        	max.clear();
-        	min.offer(x);
-        	for(int i = 0; i < n; i++) {
-        		st = new StringTokenizer(br.readLine());
-        		x = Integer.parseInt(st.nextToken());
-        		y = Integer.parseInt(st.nextToken());
-        		if(x > y) {
-        			min.offer(x);
-        			max.offer(y);
-        		}
-        		else {
-        			min.offer(y);
-        			max.offer(x);
-        		}
-        		x = min.peek();
-        		y = max.peek();
-        		if(x < y) {
-        			min.poll();
-        			max.poll();
-        			min.offer(y);
-        			max.offer(x);
-        		}
-        		
-        		res += min.peek() % 20171109;
+        for (int t = 1; t <= T; t++) {
+            int res = 0;
+            PriorityQueue<Integer> min = new PriorityQueue<>();
+            PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            max.offer(a);
+            for(int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                a = Integer.parseInt(st.nextToken());
+                if(max.peek() < a) min.offer(a);
+                else max.offer(a);
+                a = Integer.parseInt(st.nextToken());
+                if(max.peek() < a) min.offer(a);
+                else max.offer(a);
+
+                if(max.size() > min.size() + 1)
+                    min.offer(max.poll());
+                else if(min.size() > max.size())
+                    max.offer(min.poll());
+                res += max.peek();
                 res %= 20171109;
-        	}
-        	sb.append(String.format("#%d %d\n", t, res));
+            }
+            sb.append("#").append(t).append(" ").append(res).append("\n");
         }
         System.out.println(sb);
     }
