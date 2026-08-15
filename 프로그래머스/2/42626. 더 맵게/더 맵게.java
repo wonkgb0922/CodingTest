65e0edfef3a17d1bb9940918f7a52588111e921d
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-08-15 23:54:55 +0900

    [level 2] Title: 더 맵게, Time: 692.18 ms, Memory: 140 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < scoville.length;i++)
+            pq.offer(scoville[i]);
+        
+        while(pq.size() > 1 && pq.peek() < K) {
+            pq.offer(pq.poll() + pq.poll() * 2);
+            answer++;
+        }
+        if(pq.peek() < K) answer = -1;
+        return answer;
+    }
+}
