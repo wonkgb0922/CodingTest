075ffc79a69a924e3d5ab0dd7f6c5f26b14f9ad3
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-08-30 14:09:45 +0900

    [D3] Title: 이진수 표현, Time: 118 ms, Memory: 33,408 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.java
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -1,25 +1,23 @@
 import java.util.*;
 import java.io.*;
 
-class Solution
-{
-	public static void main(String[] args) throws Exception {
+public class Solution {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
-        int n, m, target;
-        String res;
+        int n, m, bin;
         for(int t = 1; t <= T; t++) {
-        	st = new StringTokenizer(br.readLine());
-        	n = Integer.parseInt(st.nextToken());
-        	m = Integer.parseInt(st.nextToken());
-        	target = (1 << n) - 1;
-        	if((m & target) == target)
-        		res = "ON";
-        	else
-        		res = "OFF";
-        	sb.append("#").append(t).append(" ").append(res).append("\n");
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            bin = (1 << n) - 1;
+            sb.append("#").append(t).append(" ");
+            if((bin & m) == bin)
+                sb.append("ON\n");
+            else
+                sb.append("OFF\n");
         }
         System.out.println(sb);
     }
