9f338fe9af4b73b9da3323f5c3f6937e9b08bb04
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-08-31 00:46:20 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 41.37 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,38 +1,34 @@
 class Solution {
-    
-    // i0% 할인
-    static int emoticons[];
-    static int ary[];
+    static int n, m;
+    static int[] ary;
     static int[][] users;
+    static int[] emoticons;
     static int max1, max2;
     
     static void dfs(int idx) {
-        if(idx >= emoticons.length) {
-            int sub = 0;
-            int val = 0;
+        if(idx == m) {
+            int cnt1 = 0, cnt2 = 0, sum;
             
-            for(int i = 0; i < users.length; i++) {
-                int sum = 0;
-                for(int j = 0; j < ary.length; j++) {
-                    if(ary[j] >= users[i][0])
-                        sum += emoticons[j] * (100 - ary[j]) / 100;
+            for(int i = 0; i < n; i++) {
+                sum = 0;
+                for(int j = 0; j < m; j++) {
+                    if(ary[j] < users[i][0]) continue;
+                    sum += emoticons[j] * (100 - ary[j]) / 100;
                 }
-                if(sum >= users[i][1]) {
-                    sub++;
-                    sum = 0;
-                }
-                val += sum;
-            }
-            if(max1 <= sub) {
-                if(max1 == sub)
-                    max2 = Math.max(max2, val);
+                if(sum >= users[i][1])
+                    cnt1++;
                 else
-                    max2 = val;
-                max1 = sub;       
+                    cnt2 += sum;                
+            }
+            if(max1 <= cnt1) {
+                if(max1 < cnt1)
+                    max2 = cnt2;
+                else max2 = Math.max(max2, cnt2);
+                max1 = cnt1;
             }
             return;
         }
-        for(int i = 1; i < 5; i++) {
+        for(int i = 1; i <= 4; i++) {
             ary[idx] = i * 10;
             dfs(idx + 1);
         }
@@ -40,9 +36,12 @@ class Solution {
     
     public int[] solution(int[][] users, int[] emoticons) {
         int[] answer = new int[2];
-        ary = new int[emoticons.length];
-        this.emoticons = emoticons;
+        n = users.length;
+        m = emoticons.length;
+        
+        ary = new int[m];
         this.users = users;
+        this.emoticons = emoticons;
         
         dfs(0);
         answer[0] = max1;
