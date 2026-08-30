8727dab30ba9e260b77275bd60e2c756cc502901
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-08-30 16:01:32 +0900

    [D4] Title: 삼성이의 쇼핑 ( 비트마스크 + 조합 연습 ), Time: 718 ms, Memory: 285,676 KB -BaekjoonHub

--- SWEA/D4/4168. 삼성이의 쇼핑 （ 비트마스크 ＋ 조합 연습 ）/삼성이의 쇼핑 （ 비트마스크 ＋ 조합 연습 ）.java
+++ SWEA/D4/4168. 삼성이의 쇼핑 （ 비트마스크 ＋ 조합 연습 ）/삼성이의 쇼핑 （ 비트마스크 ＋ 조합 연습 ）.java
@@ -3,50 +3,49 @@ import java.io.*;
 
 public class Solution {
     static int n, m;
-    static int p[], s[];
+    static int p[] = new int[25], s[] = new int[25];
+    static int dp[][];
+    static StringBuilder sb = new StringBuilder();
+    static int sol(int idx, int cost) {
+        if(idx == m) return 0;
+        if(dp[idx][cost] > 0) return dp[idx][cost];
+        dp[idx][cost] = sol(idx + 1, cost);
+        if(cost + p[idx] <= n)
+            dp[idx][cost] = Math.max(dp[idx][cost], sol(idx + 1, cost + p[idx]) + s[idx]);
+        return dp[idx][cost];
+    }
 
+    static void track(int idx, int cost) {
+        if(idx == m) {
+            return;
+        }
+        if(dp[idx][cost] == dp[idx + 1][cost]) {
+            // idx를 passing한 경우이다.
+            track(idx + 1, cost);
+        }
+        else {
+            sb.append(idx).append(" ");
+            track(idx + 1, cost + p[idx]);
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
-        int res, state;
         for(int t = 1; t <= T; t++) {
             st = new StringTokenizer(br.readLine());
             n = Integer.parseInt(st.nextToken());
             m = Integer.parseInt(st.nextToken());
-            p = new int[m];
-            s = new int[m];
             for(int i = 0; i < m; i++) {
                 st = new StringTokenizer(br.readLine());
                 p[i] = Integer.parseInt(st.nextToken());
                 s[i] = Integer.parseInt(st.nextToken());
             }
-            res = 0;
-            state = 0;
-            for(int i = 1; i < (1 << m); i++) {
-                int sum = 0, cost = 0;
-                for(int j = 0; j < m; j++) {
-                    if(((1 << j) & i) > 0) {
-                        cost += p[j];
-                        sum += s[j];
-                    }
-                    if(cost > n) {
-                        cost -= p[j];
-                        sum -= s[j];
-                        break;
-                    }
-                }
-                if(res < sum) {
-                    res = sum;
-                    state = i;
-                }
-            }
+            dp = new int[m + 1][n + 1];
+            int res = sol(0, 0);
+
             sb.append("#").append(t).append(" ");
-            for(int i = 0; i < m; i++) {
-                if((state & (1 << i)) > 0)
-                    sb.append(i).append(" ");
-            }
+            track(0, 0);
             sb.append(res).append("\n");
         }
         System.out.println(sb);
