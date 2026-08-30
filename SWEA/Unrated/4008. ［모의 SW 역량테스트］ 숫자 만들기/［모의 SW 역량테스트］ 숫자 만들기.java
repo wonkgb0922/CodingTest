d98a00f958c89cdab49ca6b732f1bf90fc56122b
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-08-30 14:47:32 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 148 ms, Memory: 32,512 KB -BaekjoonHub

--- SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
@@ -1,52 +1,51 @@
 import java.util.*;
-import java.util.function.IntBinaryOperator;
 import java.io.*;
+import java.util.function.IntBinaryOperator;
 
-public class Solution {	
-	static int n;
-	static int ary[];
-	static int max, min;
-	static IntBinaryOperator op[];
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		op = new IntBinaryOperator[4];
-		op[0] = Integer::sum;
-		op[1] = Math::subtractExact;
-		op[2] = Math::multiplyExact;
-		op[3] = (i1, i2) -> i1 / i2;
-		int state;		
-		for(int t = 1; t <= T; t++) {
-			n = Integer.parseInt(br.readLine());
-			state = 0;
-			ary = new int[n];
-			max = Integer.MIN_VALUE;
-			min = Integer.MAX_VALUE;
-			st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < 4; i++)
-				state |= Integer.parseInt(st.nextToken()) << i * 8;
-			st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < n; i++)
-				ary[i] = Integer.parseInt(st.nextToken());
-			
-			sol(0, state, ary[0]);
-			sb.append(String.format("#%d %d\n", t, max - min));
-		}
-		System.out.println(sb);
-	}
-	static void sol(int idx, int state, int cur) {
-		if(idx == n - 1) {
-			max = Math.max(max, cur);
-			min = Math.min(min, cur);
+public class Solution {
+    static int n;
+    static int ary[];
+    // 0: 합, 1: 차, 2: 곱, 3: 몫
+    static IntBinaryOperator op[] = new IntBinaryOperator[4];
+    static int max, min;
+
+    static void sol(int idx, int state, int cur) {
+        if(idx == n - 1) {
+            max = Math.max(max, cur);
+            min = Math.min(min, cur);
             return;
-		}
-		int diff = (1 << 8) - 1;
-		for(int i = 0; i < 4; i++) {
-			if((diff & (state >> (i * 8))) > 0)
-				sol(idx + 1, state - (1 << (i * 8)), op[i].applyAsInt(cur, ary[idx + 1]));
-		}
-	}
+        }
+        int diff = (1 << 8) - 1;
+        for(int i = 0; i < 4; i++) {
+            if((diff & (state >> (i * 8))) > 0)
+                sol(idx + 1, state - (1 << (i * 8)), op[i].applyAsInt(cur, ary[idx + 1]));
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        op[0] = Integer::sum;
+        op[1] = (a, b) -> a - b;
+        op[2] = (a, b) -> a * b;
+        op[3] = (a, b) -> a / b;
+        for(int t = 1; t <= T; t++) {
+            n = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            int state = 0;
+            for(int i = 0; i < 4; i++)
+                state |= Integer.parseInt(st.nextToken()) << i * 8;
+            st = new StringTokenizer(br.readLine());
+            ary = new int[n];
+            for(int i = 0; i < n; i++)
+                ary[i] = Integer.parseInt(st.nextToken());
+            max = Integer.MIN_VALUE;
+            min = Integer.MAX_VALUE;
+            sol(0, state, ary[0]);
+            sb.append("#").append(t).append(" ").append(max - min).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
