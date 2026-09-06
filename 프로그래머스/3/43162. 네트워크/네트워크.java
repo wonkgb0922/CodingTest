9848eccd60a189648c717d25f6c1ee82b355c769
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-09-06 13:15:57 +0900

    [level 3] Title: 네트워크, Time: 0.31 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    static int p[];
+    
+    static int find(int a) {
+        if(p[a] == 0) return a;
+        return p[a] = find(p[a]);
+    }
+    
+    static void merge(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if(a == b) return;
+        p[a] = b;
+    }
+    
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        p = new int[n + 1];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == j) continue;
+                if(computers[i][j] == 1) merge(i + 1, j + 1);
+            }
+        }
+        Set<Integer> set = new HashSet<>();
+        for(int i = 1; i <= n; i++)
+            set.add(find(i));
+        
+        return set.size();
+    }
+}
