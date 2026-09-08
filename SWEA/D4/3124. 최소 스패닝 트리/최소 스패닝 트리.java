212dc8bc83a51440d731c29211ae9cbcea64333f
wonkgb0922 <54229415+wonkgb0922@users.noreply.github.com>
2026-09-08 19:32:06 +0900

    [D4] Title: 최소 스패닝 트리, Time: 1,849 ms, Memory: 120,552 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,64 +2,68 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-	static int v, e;
-	static int p[];
-	
-	static int find(int a) {
-		if(p[a] < 0) return a;
-		return p[a] = find(p[a]);
-	}
-	static void merge(int a, int b) {
-		a = find(a);
-		b = find(b);
-		if(a == b) return;
-		p[a] += p[b];
-		p[b] = a;
-	}
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		long res;
-		PriorityQueue<Edge> pq = new PriorityQueue<>();;
-		for(int tc = 1; tc <= T; tc++) {
-			st = new StringTokenizer(br.readLine());
-			v = Integer.parseInt(st.nextToken());
-			e = Integer.parseInt(st.nextToken());
-			p = new int[v + 1];
-			pq.clear();
-			for(int i = 0; i < e; i++) {
-				st = new StringTokenizer(br.readLine());
-				pq.offer(new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Long.parseLong(st.nextToken())));
-			}
-			for(int i = 1; i <= v; i++)
-				p[i] = -1;
-			res = 0;
-			while(!pq.isEmpty()) {
-				Edge edge = pq.poll();
-				if(find(edge.start) == find(edge.end)) continue;
-				merge(edge.start, edge.end);
-				res += edge.w;
-				if(p[1] == -1 * v) break;
-			}
-			sb.append(String.format("#%d %d\n", tc, res));
-		}
-		System.out.print(sb);
-	}
+    static int n, m;
+    static int p[];
+    static int cnt;
+    static long res;
+
+    static int find(int a) {
+        if(p[a] == 0) return a;
+        return p[a] = find(p[a]);
+    }
+
+    static void merge(int a, int b, int w) {
+        a = find(a);
+        b = find(b);
+        if(a == b) return;
+        p[a] = b;
+        res += w;
+        cnt++;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Node> pq;
+
+        int T = Integer.parseInt(br.readLine());
+        for(int t = 1; t <= T; t++) {
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            p = new int[n + 1];
+            pq = new PriorityQueue<>();
+
+            for (int i = 0; i < m; i++) {
+                st = new StringTokenizer(br.readLine());
+                pq.offer(new Node(Integer.parseInt(st.nextToken()),
+                        Integer.parseInt(st.nextToken()),
+                        Integer.parseInt(st.nextToken())));
+            }
+            res = 0;
+            cnt = 1;
+
+            while(!pq.isEmpty() && cnt < n) {
+                Node node = pq.poll();
+                merge(node.v, node.u, node.w);
+            }
+            sb.append("#").append(t).append(" ").append(res).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
-class Edge implements Comparable<Edge> {
-	int start;
-	int end;
-	long w;
-	public Edge(int start, int end, long w) {
-		this.start = start;
-		this.end = end;
-		this.w = w;
-	}
-	@Override
-	public int compareTo(Edge o) {
-		return Long.compare(this.w, o.w);
-	}
+
+class Node implements Comparable<Node> {
+    public Node(int v, int u, int w) {
+        this.v = v;
+        this.u = u;
+        this.w = w;
+    }
+    public int compareTo(Node node) {
+        return Integer.compare(this.w, node.w);
+    }
+    public int v;
+    public int u;
+    public int w;
 }
